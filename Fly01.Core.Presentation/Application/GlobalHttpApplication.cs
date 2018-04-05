dbffee745c1f9d7cb82b49dd0036f38bf1979200
Unknown <alexisdiel@gmail.com>
2018-04-05 16:01:48 -0300

    Auto stash before merge of "Global-asax" and "master"

---
+++ Fly01.Core.Presentation/Application/GlobalHttpApplication.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Configuration;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using Fly01.Core.Config;
+using Fly01.Core.Rest;
+using Fly01.Core.VM;
+
+
+namespace Fly01.Core.Presentation.Application
+{
+    public class GlobalHttpApplication : HttpApplication
+    {
+        private static string ReadCookieAndSetSession(string token)
+        {
+            string platformUser = "";
+            var formsAuthenticationTicket = FormsAuthentication.Decrypt(token);
+
+            if ((formsAuthenticationTicket != null) && (formsAuthenticationTicket.UserData != null))
+            {
+                dynamic cookieUserData = Json.Decode(formsAuthenticationTicket.UserData);
+
+                platformUser = cookieUserData.UserName;
+                string platformUrl = cookieUserData.Fly01Url;
+
+                TokenDataVM tokenData = RestHelper.ExecuteGetAuthToken(
+                    AppDefaults.UrlGateway.Replace("v2/compras/", "v2/"), AppDefaults.GatewayUserName,
+                    AppDefaults.GatewayPassword, platformUrl, platformUser);
+
+                var userData = new UserDataVM
+                {
+                    TokenData = tokenData,
+                    PlatformUser = platformUser,
+                    PlatformUrl = platformUrl
+                };
+
+                userData.TokenData.Username = cookieUserData.Name;
+                SessionManager.Current.UserData = userData;
+            }
+            return platformUser;
+        }
+
+        protected void Application_PreRequestHandlerExecute(object sender, EventArgs e)
+        {
+            if ((Context.Handler is IRequiresSessionState || Context.Handler is IReadOnlySessionState) &&
+                SessionManager.Current.UserData.IsValidUserData() &&
+                ((HttpContext.Current.User == null) || (HttpContext.Current.User.Identity.IsAuthenticated == false)))
+            {
+                HttpContext.Current.Session.Clear();
+                HttpContext.Current.Session.Abandon();
+                HttpContext.Current.Session.RemoveAll();
+                FormsAuthentication.SignOut();
+
+                if (Request.Headers["X-Requested-With"] != null &&
+                    Request.Headers["X-Requested-With"].ToUpper().Equals("XMLHTTPREQUEST"))
+                    FormsAuthentication.RedirectToLoginPage();
+                else
+                {
+                    Response.Write(
+                        String.Format("<script type=\"text/javascript\">top.location.href='{0}';</script>",
+                            FormsAuthentication.LoginUrl));
+                    Response.End();
+                }
+            }
+            else
+            {
+                if (FormsAuthentication.CookiesSupported && Request.Cookies[FormsAuthentication.FormsCookieName] != null)
+                {
+                    HttpContext.Current.User =
+                        new GenericPrincipal(
+                            new GenericIdentity(
+                                ReadCookieAndSetSession(Request.Cookies[FormsAuthentication.FormsCookieName].Value),
+                                "Forms"),
+                            string.Empty.Split(';'));
+                }
+            }
+        }
+
+        protected void Application_Start()
+        {
+            AppDefaults.MashupClientId = ConfigurationManager.AppSettings["MashupClientId"];
+            AppDefaults.MashupPassword = ConfigurationManager.AppSettings["MashupPassword"];
+            AppDefaults.MashupUser = ConfigurationManager.AppSettings["MashupUser"];
+            AppDefaults.UrlGateway = ConfigurationManager.AppSettings["UrlS1Gateway"];
+            AppDefaults.GatewayUserName = ConfigurationManager.AppSettings["GatewayUserName"];
+            AppDefaults.GatewayPassword = ConfigurationManager.AppSettings["GatewayPassword"];
+            AppDefaults.GatewayVerificationKeyPassword = ConfigurationManager.AppSettings["GatewayVerificationKeyPassword"];
+            AppDefaults.UrlLoginSSO = ConfigurationManager.AppSettings["UrlLoginSSO"];
+            AppDefaults.UrlLogoutSSO = ConfigurationManager.AppSettings["UrlLogoutSSO"];
+            AppDefaults.UrlLicenseManager = ConfigurationManager.AppSettings[""];
+            AppDefaults.SessionKey = ConfigurationManager.AppSettings["SessionKey"];
+            AppDefaults.AppIdCompras = ConfigurationManager.AppSettings["AppIdCompras"];
+            AppDefaults.AppIdFaturamento = ConfigurationManager.AppSettings["AppIdFaturamento"];
+            AppDefaults.AppIdCompras = ConfigurationManager.AppSettings["AppIdCompras"];
+            AppDefaults.AppIdEstoque = ConfigurationManager.AppSettings["AppIdEstoque"];
+            AppDefaults.AppIdSaude = ConfigurationManager.AppSettings["AppIdSaude"];
+            AppDefaults.RootPathApplication = ConfigurationManager.AppSettings["RootPathApplication"];
+
+            //AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //BundleTable.EnableOptimizations = false;
+
+            AntiForgeryConfig.SuppressIdentityHeuristicChecks = true;
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            if ((Request.Cookies["ASP.NET_SessionId"] != null) &&
+                (Request.Url.LocalPath.Equals(FormsAuthentication.LoginUrl,
+                    StringComparison.InvariantCultureIgnoreCase)))
+                return;
+            if (FormsAuthentication.CookiesSupported &&
+                Request.Cookies[FormsAuthentication.FormsCookieName] != null)
+            {
+                HttpContext.Current.User =
+                    new GenericPrincipal(
+                        new GenericIdentity(
+                            ReadCookieAndSetSession(Request.Cookies[FormsAuthentication.FormsCookieName].Value), "Forms"),
+                        string.Empty.Split(';'));
+            }
+            else
+            {
+                HttpContext.Current.Session.Clear();
+                HttpContext.Current.Session.Abandon();
+                HttpContext.Current.Session.RemoveAll();
+                FormsAuthentication.SignOut();
+
+                if (Request.Headers["X-Requested-With"] != null &&
+                        Request.Headers["X-Requested-With"].ToUpper().Equals("XMLHTTPREQUEST"))
+                {
+                    FormsAuthentication.RedirectToLoginPage();
+                }
+                else if (Request.AppRelativeCurrentExecutionFilePath.Equals("~/") && Request.QueryString["t"] != null)
+                {
+                    Response.Write(String.Format("<script type=\"text/javascript\">top.location.href='{0}?t={1}';</script>", FormsAuthentication.LoginUrl, Request.QueryString["t"]));
+                    Response.End();
+                }
+                else
+                {
+                    Response.Write(String.Format("<script type=\"text/javascript\">top.location.href='{0}';</script>", FormsAuthentication.LoginUrl));
+                    Response.End();
+                }
+            }
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+            FormsAuthentication.SignOut();
+        }
+
+    }
+}

c84b20f201ca6e8f41db8e18139f740e8a0f2451
ISRAEL MACHADO DE BORBA <israel.borba@poa01.local>
2018-04-05 15:48:16 -0300

    Referências Fly01.Financeiro.DAL

--- appfinanceiro/Fly01.Financeiro.DAL/AppDataContext.cs
+++ appfinanceiro/Fly01.Financeiro.DAL/AppDataContext.cs
@@ -1,8 +1,8 @@
 ﻿using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
-using Fly01.Core;
 using Fly01.Core.Base;
 using Fly01.Financeiro.Domain.Entities;
+using Fly01.Core.Domain;
 
 namespace Fly01.Financeiro.API.Models.DAL
 {

a2872974a978dd88a352e3a384ba1848c4b66e25
israel.borba <israel.borba@POAD040100298>
2018-04-09 10:03:39 -0300

    Todos os projetos (Presentation)
    Equalização de versões web.config

---
+++ Fly01.Core.Domain/Entities/CategoriaBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fly01.Core.Domain.Base
+{
+    class CategoriaBase
+    {
+    }
+}

438a1e44d8f820035e522314d487aaa66b15d2ce
Jefferson de Oliveira <jeff-Dias@outlook.com>
2018-04-10 19:52:39 -0300

    Refatoração do TransmissãoBL e criação de classes de transporte

--- appemissaoNFE/Fly01.EmissaoNFE.Domain/Entities/NFe/Transporte.cs
+++ appemissaoNFE/Fly01.EmissaoNFE.Domain/Entities/NFe/Transporte.cs
@@ -3,10 +3,10 @@ using System.Xml.Serialization;
 
 namespace Fly01.EmissaoNFE.Domain.Entities.NFe
 {
-    [XmlRoot(ElementName = "transp")]
+    [XmlRoot("transp")]
     public class Transporte
     {
-        [XmlElement(ElementName = "modFrete")]
+        [XmlElement("modFrete")]
         public ModalidadeFrete ModalidadeFrete { get; set; }
 
         [XmlElement(ElementName = "transporta", IsNullable = true)]
@@ -16,5 +16,22 @@ namespace Fly01.EmissaoNFE.Domain.Entities.NFe
         {
             return Transportadora != null;
         }
+
+        [XmlElement(ElementName = "veicTransp", IsNullable = true)]
+        public Veiculo Veiculo { get; set; }
+
+        public bool ShouldSerializeVeiculo()
+        {
+            return Veiculo != null;
+        }
+
+        [XmlElement(ElementName = "vol", IsNullable = true)]
+        public Volume Volume { get; set; }
+
+        public bool ShouldSerializeVolume()
+        {
+            return Volume != null;
+        }
+
     }
 }

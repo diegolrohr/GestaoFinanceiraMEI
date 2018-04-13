8732011a8efd8701a2ee129f079bef53849d3e59
tiagopariz <tiagopariz@gmail.com>
2018-04-13 15:43:38 -0300

    Campos FCP em ICMS

--- appemissaoNFE/Fly01.EmissaoNFE.Domain/Entities/NFe/ICMS/ICMS.cs
+++ appemissaoNFE/Fly01.EmissaoNFE.Domain/Entities/NFe/ICMS/ICMS.cs
@@ -9,7 +9,7 @@ namespace Fly01.EmissaoNFE.Domain.Entities.NFe.ICMS
         {
 
         }
-        
+
         public ICMS(OrigemMercadoria origemMercadoria, CSOSN codigoSituacaoOperacao)
         {
             OrigemMercadoria = origemMercadoria;
@@ -21,6 +21,52 @@ namespace Fly01.EmissaoNFE.Domain.Entities.NFe.ICMS
 
         [XmlElement(ElementName = "CSOSN")]
         public CSOSN CodigoSituacaoOperacao { get; set; }
-       
+
+        #region FCP
+
+        /// <summary>
+        /// Valor da Base de Cálculo do FCP retido por Substituição Tributária
+        /// </summary>
+        [XmlElement(ElementName = "vBCFCPST")]
+        public double ValorBaseFCPRetidoST { get; set; }
+
+        /// <summary>
+        /// Percentual do FCP retido por Substituição Tributária
+        /// </summary>
+        [XmlElement(ElementName = "pFCPST")]
+        public double PercentualFCPRetidoST { get; set; }
+
+        /// <summary>
+        /// Valor do FCP retido por Substituição Tributária
+        /// </summary>
+        [XmlElement(ElementName = "vFCPST")]
+        public double ValorFCPST { get; set; }
+
+        /// <summary>
+        /// Alíquota suportada pelo Consumidor Final
+        /// </summary>
+        [XmlElement(ElementName = "pST")]
+        public double AliquotaFCPConsumidorFinal { get; set; }
+
+        /// <summary>
+        /// Valor da Base de Cálculo do FCP retido anteriormente por ST
+        /// </summary>
+        [XmlElement(ElementName = "vBCFCPSTRet")]
+        public double ValorBaseFCPRetidoAnteriorST { get; set; }
+
+        /// <summary>
+        /// Percentual do FCP retido anteriormente por Substituição Tributária
+        /// </summary>
+        [XmlElement(ElementName = "vFCPSTRet")]
+        public double PercentualFCPRetidoAnteriorST { get; set; }
+
+        /// <summary>
+        /// Valor do FCP retido por Substituição Tributária
+        /// </summary>
+        [XmlElement(ElementName = "vFCPSTRet")]
+        public double ValorFCPRetidoST { get; set; }
+
+        #endregion FCP
+
     }
 }

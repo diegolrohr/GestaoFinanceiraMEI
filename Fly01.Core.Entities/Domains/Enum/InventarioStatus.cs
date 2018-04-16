7ad60cfb816938e2e4332919371808e61b071fb7
israel.borba <israel.borba@totvs.com.br>
2018-04-16 15:35:00 -0300

    Refactor TipoEntradaSaida enum

--- Fly01.Core.Entities/Domains/Enum/InventarioStatus.cs
+++ Fly01.Core.Entities/Domains/Enum/InventarioStatus.cs
@@ -1,11 +1,12 @@
 ﻿using Fly01.Core.Entities.Attribute;
 
-namespace Fly01.Estoque.Domain.Enums
+namespace Fly01.Core.Entities.Domains.Enum
 {
     public enum InventarioStatus
     {
         [Subtitle("Aberto", "ABERTO", "ABERTO", "green")]
         Aberto = 1,
+
         [Subtitle("Finalizado", "FINALIZADO", "FINALIZADO", "red")]
         Finalizado = 2
     }

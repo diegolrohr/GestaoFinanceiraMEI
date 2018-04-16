4df59c63d48830e309d29c517dfcb1d49ae26162
israel.borba <israel.borba@totvs.com.br>
2018-04-16 11:33:20 -0300

    Refactor enum TipoPessoa

--- Fly01.Core.Entities/Domains/Enum/TipoPessoa.cs
+++ Fly01.Core.Entities/Domains/Enum/TipoPessoa.cs
@@ -1,11 +1,12 @@
 ﻿using Fly01.Core.Entities.Attribute;
 
-namespace Fly01.Compras.Domain.Enums
+namespace Fly01.Core.Entities.Domains.Enum
 {
     public enum TipoPessoa
     {
         [Subtitle("Fisica", "Física")]
         Fisica = 1,
+
         [Subtitle("Juridica", "Jurídica")]
         Juridica = 2
     }

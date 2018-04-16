0450fa06bf02790f5b3332418d948282ac70aa5a
israel.borba <israel.borba@totvs.com.br>
2018-04-16 11:37:05 -0300

    Refactor enum TipoPeriodicidade

--- Fly01.Core.Entities/Domains/Enum/TipoPeriodicidade.cs
+++ Fly01.Core.Entities/Domains/Enum/TipoPeriodicidade.cs
@@ -1,15 +1,18 @@
 ﻿using Fly01.Core.Entities.Attribute;
 
-namespace Fly01.Compras.Domain.Enums
+namespace Fly01.Core.Entities.Domains.Enum
 {
     public enum TipoPeriodicidade
     {
         [Subtitle("Nenhuma", "Nenhuma")]
         Nenhuma = 0,
+
         [Subtitle("Semanal", "Semanal")]
         Semanal = 1,
+
         [Subtitle("Mensal", "Mensal")]
         Mensal = 2,
+
         [Subtitle("Anual", "Anual")]
         Anual = 3
     }

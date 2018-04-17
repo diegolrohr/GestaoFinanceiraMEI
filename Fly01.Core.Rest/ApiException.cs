1f149cc21ce8d6a1e262bed09f047762a6f21048
Alexis Diel <alexisdiel@gmail.com>
2018-04-17 12:21:37 -0300

    Auto stash before merge of "RemoveSaasCommons" and "master"

---
+++ Fly01.Core.Rest/ApiException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+
+namespace Fly01.Core.Rest
+{
+    public class ApiException : Exception
+    {
+        private HttpStatusCode statusCode = HttpStatusCode.OK;
+        public HttpStatusCode StatusCode
+        {
+            get
+            {
+                if (statusCode == 0)
+                    statusCode = HttpStatusCode.InternalServerError;
+
+                return statusCode;
+            }
+
+            set { statusCode = value; }
+        }
+
+        public ApiException(HttpStatusCode statusCode, string message, Exception ex)
+            : base(message, ex)
+        {
+            this.statusCode = statusCode;
+        }
+
+        public ApiException(HttpStatusCode statusCode, string message)
+            : base(message)
+        {
+            this.statusCode = statusCode;
+        }
+
+        public ApiException(HttpStatusCode statusCode)
+        {
+            this.statusCode = statusCode;
+        }
+    }
+}

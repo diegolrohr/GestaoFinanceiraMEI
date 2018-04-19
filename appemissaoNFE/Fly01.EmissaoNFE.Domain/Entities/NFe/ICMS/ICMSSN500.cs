6941f5809e89cfd82c260a979ab578fdbaa67510
Jefferson de Oliveira <jeff-Dias@outlook.com>
2018-04-19 09:47:00 -0300

    Implementação de serialização e validações de FCP

--- appemissaoNFE/Fly01.EmissaoNFE.Domain/Entities/NFe/ICMS/ICMSSN500.cs
+++ appemissaoNFE/Fly01.EmissaoNFE.Domain/Entities/NFe/ICMS/ICMSSN500.cs
@@ -4,7 +4,7 @@ using System.Xml.Serialization;
 
 namespace Fly01.EmissaoNFE.Domain.Entities.NFe.ICMS
 {
-    [XmlRoot(ElementName = "ICMSSN500")]
+    [XmlRoot("ICMSSN500")]
     public class ICMSSN500 : ICMS
     {
         public ICMSSN500()
@@ -16,10 +16,19 @@ namespace Fly01.EmissaoNFE.Domain.Entities.NFe.ICMS
         {
         }
         
-        [XmlElement(ElementName = "vBCSTRet")]
+        [XmlElement("vBCSTRet")]
         public double ValorBCSTRetido { get; set; }
 
-        [XmlElement(ElementName = "vICMSSTRet")]
+        [XmlElement("vICMSSTRet")]
         public double ValorICMSSTRetido { get; set; }
+
+        [XmlElement("vBCFCPSTRet")]
+        public double? BaseFCPSTRetido { get; set; }
+
+        [XmlElement("pFCPSTRet")]
+        public double? AliquotaFCPSTRetido { get; set; }
+
+        [XmlElement("vFCPSTRet")]
+        public double? ValorFCPSTRetido { get; set; }
     }
 }

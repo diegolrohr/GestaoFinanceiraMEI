2078cf30aafa6118c6589778f100a9c874e58c64
tiagopariz <tiagopariz@gmail.com>
2018-04-27 16:57:17 -0300

    Migration para as novas colunas em Certificado

--- appfaturamento/Fly01.Faturamento.API/Controllers/Api/CertificadoDigitalController.cs
+++ appfaturamento/Fly01.Faturamento.API/Controllers/Api/CertificadoDigitalController.cs
@@ -1,12 +1,15 @@
-﻿using Fly01.Faturamento.BL;
+﻿using Fly01.Core;
 using Fly01.Core.Entities.Domains.Commons;
-using System.Web.OData.Routing;
+using Fly01.Core.Notifications;
+using Fly01.Core.Reports;
+using Fly01.Core.Rest;
+using Fly01.Faturamento.BL;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http;
-using System;
 using System.Web.OData;
-using System.Linq;
-using Fly01.Core.Notifications;
+using System.Web.OData.Routing;
 
 namespace Fly01.Faturamento.API.Controllers.Api
 {
@@ -15,7 +18,7 @@ namespace Fly01.Faturamento.API.Controllers.Api
     {
         public override async Task<IHttpActionResult> Put([FromODataUri] Guid key, Delta<CertificadoDigital> model)
         {
-            using (UnitOfWork unitOfWork = new UnitOfWork(ContextInitialize))
+            using (var unitOfWork = new UnitOfWork(ContextInitialize))
             {
                 if (!unitOfWork.CertificadoDigitalBL.All.Any())
                     return BadRequest("Nenhum certificado foi encontrado para esta plataforma.");
@@ -37,13 +40,37 @@ namespace Fly01.Faturamento.API.Controllers.Api
         {
             try
             {
-                using (UnitOfWork unitOfWork = new UnitOfWork(ContextInitialize))
+                using (var unitOfWork = new UnitOfWork(ContextInitialize))
                 {
                     if (unitOfWork.CertificadoDigitalBL.All.Any())
                         return BadRequest("Já existe um certificado cadastrado para esta plataforma.");
 
                     entity = unitOfWork.CertificadoDigitalBL.ProcessEntity(entity);
                     return await base.Post(entity);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new BusinessException(ex.Message);
+            }
+        }
+
+        public override IHttpActionResult Get()
+        {
+            try
+            {
+                using (var unitOfWork = new UnitOfWork(ContextInitialize))
+                {
+                    var dadosEmpresa = RestHelper.ExecuteGetRequest<ManagerEmpresaVM>($"{AppDefaults.UrlGateway}v2/", $"Empresa/{PlataformaUrl}");
+                    var empresaCnpj = dadosEmpresa.CNPJ;
+                    var empresaIE = dadosEmpresa.InscricaoEstadual;
+                    var empresaUF = dadosEmpresa.Cidade != null ? (dadosEmpresa.Cidade.Estado != null ? dadosEmpresa.Cidade.Estado.Sigla : "") : "";
+
+                    // TODO: Comparar com o certificado
+                    // TODO: Excluir o certificado em faturamento.
+                    //RestUtils.ExecuteDeleteRequest($"{urlFaturamentoApi}CertificadoDigital/{certificadoId}", "CertificadoDigital", null);
+
+                    return base.Get();
                 }
             }
             catch (Exception ex)

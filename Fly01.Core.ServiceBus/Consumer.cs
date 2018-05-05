5c1e871adeb2d0a968835b6d207f11c566de26c3
Jefferson Follmann <jeff.follmann@gmail.com>
2018-05-04 21:12:41 -0300

    Rabbit: Mensagens em lote (v2)

--- Fly01.Core.ServiceBus/Consumer.cs
+++ Fly01.Core.ServiceBus/Consumer.cs
@@ -1,85 +1,107 @@
 ﻿using System;
 using System.Text;
 using RabbitMQ.Client;
-using Newtonsoft.Json.Linq;
 using RabbitMQ.Client.Events;
 using System.Threading.Tasks;
 using Fly01.Core.Mensageria;
+using System.Collections.Generic;
 
 namespace Fly01.Core.ServiceBus
 {
     public abstract class Consumer
     {
-        private string Username
+        private IConnection _connection;
+        protected string Message;
+        protected RabbitConfig.enHTTPVerb HTTPMethod;
+        protected Dictionary<string, object> Headers = new Dictionary<string, object>();
+        protected abstract Task PersistMessage();
+
+        //private string Username
+        //{
+        //    get
+        //    {
+        //        if (Headers.Count == 0)
+        //            return string.Empty;
+
+        //        return Headers["AppUser"].ToString();
+        //        //var userName = string.Empty;
+        //        //if (string.IsNullOrEmpty(Message))
+        //        //    return userName;
+
+        //        //userName =
+        //        //    HTTPMethod == RabbitConfig.enHTTPVerb.POST ? JObject.Parse(Message).GetValue("usuarioInclusao")?.ToString()
+        //        //    : HTTPMethod == RabbitConfig.enHTTPVerb.PUT ? JObject.Parse(Message).GetValue("usuarioAlteracao")?.ToString()
+        //        //    : JObject.Parse(Message).GetValue("usuarioExclusao")?.ToString();
+
+        //        //return userName;
+        //    }
+        //}
+
+        //private string Plataforma
+        //{
+        //    get
+        //    {
+        //        if(Headers.Count == 0)
+        //            return string.Empty;
+
+        //        return Headers["PlataformaUrl"].ToString();
+        //    }
+        //}
+        
+        private IConnection Connection
         {
             get
             {
-                var userName = string.Empty;
-                if (string.IsNullOrEmpty(Message))
-                    return userName;
+                if (_connection == null)
+                    _connection = RabbitConfig.Factory.CreateConnection("cnsm" + RabbitConfig.QueueName);
 
-                userName =
-                    HTTPMethod == RabbitConfig.enHTTPVerb.POST ? JObject.Parse(Message).GetValue("usuarioInclusao")?.ToString()
-                    : HTTPMethod == RabbitConfig.enHTTPVerb.PUT ? JObject.Parse(Message).GetValue("usuarioAlteracao")?.ToString()
-                    : JObject.Parse(Message).GetValue("usuarioExclusao")?.ToString();
-
-                return userName;
+                return _connection;
             }
         }
 
-        private string Plataforma
+        private IModel Channel
         {
             get
             {
-                if (string.IsNullOrEmpty(Message))
-                    return string.Empty;
-
-                return JObject.Parse(Message).GetValue("plataformaId")?.ToString();
-            }
-        }
-
-        private IConnection conn;
-        private IConnection Conn
-        {
-            get
-            {
-                if (conn == null)
-                    conn = RabbitConfig.Factory.CreateConnection("cnsm" + RabbitConfig.QueueName);
-
-                return conn;
-            }
-        }
-
-        private IModel channel
-        {
-            get
-            {
-                var model = Conn.CreateModel();
+                var model = Connection.CreateModel();
                 return model;
             }
         }
 
-        protected string Message;
-        protected RabbitConfig.enHTTPVerb HTTPMethod;
+        private bool HeaderIsValid()
+        {
+            return !string.IsNullOrWhiteSpace(GetHeaderValue("PlataformaUrl")) &&
+                !string.IsNullOrWhiteSpace(GetHeaderValue("AppUser"));
+        }
+
+        private string GetHeaderValue(string key)
+        {
+            if(!Headers.ContainsKey(key))
+                return string.Empty;
+
+            return Encoding.UTF8.GetString(Headers[key] as byte[]);
+        }
 
         public void Consume()
         {
-            var consumer = new EventingBasicConsumer(channel);
-
-            consumer.Received += async (sender, ea) =>
+            var consumer = new EventingBasicConsumer(Channel);
+            
+            consumer.Received += async (sender, args) =>
             {
-                if (ea.BasicProperties.AppId != RabbitConfig.AppId)
+                if (args.BasicProperties.AppId != RabbitConfig.AppId)
                 {
-                    var properties = ea.BasicProperties;
-
                     try
                     {
-                        Message = Encoding.UTF8.GetString(ea.Body);
-                        HTTPMethod = (RabbitConfig.enHTTPVerb)Enum.Parse(typeof(RabbitConfig.enHTTPVerb), ea.BasicProperties?.Type ?? "PUT");
-                        
-                        RabbitConfig.PlataformaUrl = Plataforma;
-                        RabbitConfig.AppUser = Username;
-                        RabbitConfig.RoutingKey = ea.RoutingKey ?? "";
+                        if (args.BasicProperties.Headers == null || !HeaderIsValid())
+                            throw new ArgumentException("A 'PlataformaUrl' e o 'AppUser' devem ser informados no Header da request");
+
+                        Headers = new Dictionary<string, object>(args.BasicProperties.Headers);
+                        Message = Encoding.UTF8.GetString(args.Body);
+                        HTTPMethod = (RabbitConfig.enHTTPVerb)Enum.Parse(typeof(RabbitConfig.enHTTPVerb), args.BasicProperties?.Type ?? "PUT");
+
+                        RabbitConfig.PlataformaUrl = GetHeaderValue("PlataformaUrl");
+                        RabbitConfig.AppUser = GetHeaderValue("AppUser");
+                        RabbitConfig.RoutingKey = args.RoutingKey ?? string.Empty;
 
                         await PersistMessage();
                     }
@@ -91,18 +113,16 @@ namespace Fly01.Core.ServiceBus
                             RabbitConfig.Factory?.VirtualHost,
                             RabbitConfig.QueueName
                             );
-                        channel.BasicNack(ea.DeliveryTag, false, true);
+                        Channel.BasicNack(args.DeliveryTag, false, true);
                     }
                     finally
                     {
-                        channel.BasicAck(ea.DeliveryTag, false);
+                        Channel.BasicAck(args.DeliveryTag, false);
                     }
                 }
             };
 
-            channel.BasicConsume(RabbitConfig.QueueName, true, consumer);
+            Channel.BasicConsume(RabbitConfig.QueueName, true, consumer);
         }
-
-        protected abstract Task PersistMessage();
     }
 }
--- Fly01.Core.ServiceBus/Producer.cs
+++ Fly01.Core.ServiceBus/Producer.cs
@@ -4,12 +4,13 @@ using Newtonsoft.Json;
 using System;
 using Fly01.Core.Mensageria;
 using Fly01.Core.Entities.Domains;
+using System.Collections.Generic;
 
 namespace Fly01.Core.ServiceBus
 {
     public class Producer<TEntity> where TEntity : DomainBase
     {
-        public static void Send(string routingKey, TEntity message, RabbitConfig.enHTTPVerb httpVerb)
+        public static void Send(string routingKey, string appUser, string plataformaUrl, TEntity message, RabbitConfig.enHTTPVerb httpVerb)
         {
             try
             {
@@ -21,6 +22,12 @@ namespace Fly01.Core.ServiceBus
                         properties.Persistent = true;
                         properties.AppId = RabbitConfig.AppId;
                         properties.Type = httpVerb.ToString();
+
+                        properties.Headers = new Dictionary<string, object>()
+                        {
+                            { "AppUser", appUser },
+                            { "PlataformaUrl", plataformaUrl },
+                        };
                         
                         channel.BasicPublish(RabbitConfig.AMQPExchange, routingKey, properties, Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message)));
                     }

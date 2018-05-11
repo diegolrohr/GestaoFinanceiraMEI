aa8e41565d1684ff11410949f0a8b4cadd37cf8d
Unknown <cassio.tomilin@gmail.com>
2018-05-11 13:12:27 -0300

    Criacao avaliacaoapp no compras

--- Fly01.Core.Presentation/Controllers/AvaliacaoAppBaseController.cs
+++ Fly01.Core.Presentation/Controllers/AvaliacaoAppBaseController.cs
@@ -1,4 +1,6 @@
-﻿using Fly01.Core.Presentation.Commons;
+﻿using Fly01.Core.Helpers;
+using Fly01.Core.Presentation.Commons;
+using Fly01.Core.Rest;
 using Fly01.Core.ViewModels.Presentation.Commons;
 using Fly01.uiJS.Classes;
 using Fly01.uiJS.Classes.Elements;
@@ -18,15 +20,14 @@ namespace Fly01.Core.Presentation.Controllers
             {
                 History = new ContentUIHistory
                 {
-                    Default = Url.Action("Create"),
-                    WithParams = Url.Action("Edit")
+                    Default = Url.Action("Index")
                 },
                 Header = new HtmlUIHeader
                 {
                     Title = "Avalie o Aplicativo",
                     Buttons = new List<HtmlUIButton>
                     {
-                        new HtmlUIButton { Id = "save", Label = "Salvar", OnClickFn = "fnSalvar", Type = "submit" }
+                        new HtmlUIButton { Id = "save", Label = "Enviar", OnClickFn = "fnSalvar", Type = "submit" }
                     }
                 },
                 UrlFunctions = Url.Action("Functions") + "?fns="
@@ -37,16 +38,17 @@ namespace Fly01.Core.Presentation.Controllers
                 Action = new FormUIAction
                 {
                     Create = @Url.Action("Create"),
-                    Edit = @Url.Action("Edit"),
-                    Get = @Url.Action("Json") + "/",
-                    List = Url.Action("List")
+                    List = Url.Action("Form")
                 },
                 UrlFunctions = Url.Action("Functions") + "?fns="
             };
 
             config.Elements.Add(new InputHiddenUI { Id = "id" });
-            config.Elements.Add(new InputTextUI { Id = "descricao", Class = "col s12 m12 l9", Label = "Descrição", Required = true, MaxLength = 40 });
-            config.Elements.Add(new InputTextUI { Id = "contato", Class = "col s12 l3", Label = "Pessoa de Contato", MaxLength = 45 });
+            config.Elements.Add(new InputTextUI { Id = "titulo", Class = "col s12 l3", Label = "Título", Required = true, MaxLength = 45 });
+            config.Elements.Add(new InputTextUI { Id = "descricao", Class = "col s12 m12 24", Label = "Descrição", Required = true });
+            config.Elements.Add(new InputHiddenUI { Id = "menu", Value= "AvaliaçãoAPP" });
+            config.Elements.Add(new InputTextUI { Id = "satisfacao", Class = "col s12 l3", Label = "Satisfação", Required = true });
+            config.Elements.Add(new InputHiddenUI { Id = "aplicativo", Class = "col s12 l3", Label = "Aplicativo"});
 
             cfg.Content.Add(config);
             return Content(JsonConvert.SerializeObject(cfg, JsonSerializerSetting.Default), "application/json");
@@ -60,6 +62,24 @@ namespace Fly01.Core.Presentation.Controllers
         public override ContentResult List()
         {
             return Form();
+        }
+
+        [HttpPost]
+        public override JsonResult Create(T entityVM)
+        {
+            try
+            {
+                var postResponse = RestHelper.ExecutePostRequest(ResourceName, JsonConvert.SerializeObject(entityVM, JsonSerializerSetting.Default));
+                T postResult = JsonConvert.DeserializeObject<T>(postResponse);
+                var response = new JsonResult();
+                response.Data = new { success = true, message = "Avaliação enviada com sucesso."};
+                return (response);
+            }
+            catch (Exception ex)
+            {
+                var error = JsonConvert.DeserializeObject<ErrorInfo>(ex.Message);
+                return JsonResponseStatus.GetFailure(error.Message);
+            }
         }
     }
 }
--- appcompras/Fly01.Compras/Controllers/AvaliacaoAppController.cs
+++ appcompras/Fly01.Compras/Controllers/AvaliacaoAppController.cs
@@ -1,9 +1,21 @@
-﻿using Fly01.Core.Presentation.Controllers;
+﻿using System.Web.Mvc;
+using Fly01.Core;
+using Fly01.Core.Presentation.Controllers;
 using Fly01.Core.ViewModels.Presentation;
 
 namespace Fly01.Compras.Controllers
 {
     public class AvaliacaoAppController : AvaliacaoAppBaseController<AvaliacaoAppVM>
     {
+        public AvaliacaoAppController()
+        {
+            ResourceName = AppDefaults.GetResourceName(typeof(AvaliacaoAppVM));
+        }
+
+        public override JsonResult Create(AvaliacaoAppVM entityVM)
+        {
+            entityVM.Aplicativo = "Compras";
+            return base.Create(entityVM);
+        }
     }
 }

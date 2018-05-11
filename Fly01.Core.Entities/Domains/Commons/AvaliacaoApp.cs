3a634fe14a97290517c55fbace815ab3c6ab8172
Unknown <cassio.tomilin@gmail.com>
2018-05-11 13:33:10 -0300

    criacao avaliacaoapp do estoque

--- Fly01.Core.Entities/Domains/Commons/AvaliacaoApp.cs
+++ Fly01.Core.Entities/Domains/Commons/AvaliacaoApp.cs
@@ -5,23 +5,18 @@ namespace Fly01.Core.Entities.Domains.Commons
 {
     public class AvaliacaoApp : PlataformaBase
     {
-        [Required]
-        [StringLength(500, ErrorMessage = "O campo {0} deve possuir entre {2} e {1} caracteres.")]
         [JsonProperty("descricao")]
         public string Descricao { get; set; }
 
         [JsonProperty("titulo")]
         public string Titulo { get; set; }
 
-        [Required]
         [JsonProperty("menu")]
         public string Menu { get; set; }
 
-        [Required]
         [JsonProperty("satisfacao")]
         public int Satisfacao { get; set; }
 
-        [Required]
         [JsonProperty("aplicativo")]
         public string Aplicativo { get; set; }
     }

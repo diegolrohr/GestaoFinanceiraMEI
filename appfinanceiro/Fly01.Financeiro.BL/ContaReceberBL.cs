8f400281218d727925c6beb290bcc272fe1253ae
tiagopariz <tiagopariz@gmail.com>
2018-05-25 14:30:21 -0300

    Corrija o cadastro de recorrência com parcelas
    Ao cadastrar uma conta a receber com recorrência, o sistema não estava
    defindo a conta pai de recorrência para as primeiras parcelas.
    
    Portanto foi definido que quando a conta não for pai, ele preche o campo
    de pai da recorrência.
    
    Resolve: #841

--- appfinanceiro/Fly01.Financeiro.BL/ContaReceberBL.cs
+++ appfinanceiro/Fly01.Financeiro.BL/ContaReceberBL.cs
@@ -1,12 +1,12 @@
-﻿using Fly01.Financeiro.API.Models.DAL;
-using Fly01.Core.BL;
+﻿using Fly01.Core.BL;
+using Fly01.Core.Entities.Domains.Commons;
+using Fly01.Core.Entities.Domains.Enum;
 using Fly01.Core.Helpers;
 using Fly01.Core.Notifications;
+using Fly01.Financeiro.API.Models.DAL;
 using System;
-using System.Linq;
-using Fly01.Core.Entities.Domains.Enum;
 using System.Data.Entity;
-using Fly01.Core.Entities.Domains.Commons;
+using System.Linq;
 
 namespace Fly01.Financeiro.BL
 {
@@ -34,22 +34,10 @@ namespace Fly01.Financeiro.BL
 
         public override void Insert(ContaReceber entity)
         {
-            const int limiteSemanal = 208;
-            const int limiteMensal = 48;
-            const int limiteAnual = 4;
-
             entity.PlataformaId = PlataformaUrl;
             entity.UsuarioInclusao = AppUser;
 
-            entity.Fail(entity.Repetir && entity.TipoPeriodicidade == TipoPeriodicidade.Nenhuma, TipoPeriodicidadeInvalida);
-            entity.Fail(entity.Repetir && !entity.NumeroRepeticoes.HasValue, NumeroRepeticoesInvalido);
-
-            entity.Fail((entity.Repetir && entity.TipoPeriodicidade != TipoPeriodicidade.Nenhuma && entity.NumeroRepeticoes.HasValue) &&
-                ((entity.TipoPeriodicidade == TipoPeriodicidade.Semanal && !(entity.NumeroRepeticoes.Value > 0 && entity.NumeroRepeticoes.Value <= limiteSemanal)) ||
-                (entity.TipoPeriodicidade == TipoPeriodicidade.Mensal && !(entity.NumeroRepeticoes.Value > 0 && entity.NumeroRepeticoes.Value <= limiteMensal)) ||
-                (entity.TipoPeriodicidade == TipoPeriodicidade.Anual && !(entity.NumeroRepeticoes.Value > 0 && entity.NumeroRepeticoes.Value <= limiteAnual)))
-            , RepeticoesInvalidas);
-
+            var repetir = RepeticaoValida(entity);
 
             //ContaFinanceira.Número
             var max = Everything.Any(x => x.Id != entity.Id) ? Everything.Max(x => x.Numero) : 0;
@@ -80,13 +68,21 @@ namespace Fly01.Financeiro.BL
                 itemContaReceber.DescricaoParcela = parcela.DescricaoParcela;
                 itemContaReceber.ValorPrevisto = parcela.Valor;
 
-                itemContaReceber.Id = iParcela == default(int) ? contaFinanceiraPrincipal : default(Guid);
+                if (iParcela == default(int))
+                {
+                    itemContaReceber.Id = contaFinanceiraPrincipal;
+                }
+                else
+                {
+                    itemContaReceber.Id = Guid.NewGuid();
+                    itemContaReceber.ContaFinanceiraRepeticaoPaiId = contaFinanceiraPrincipal;
+                }
 
                 itemContaReceber.Numero = ++max;
 
                 base.Insert(itemContaReceber);
 
-                if (entity.Repetir && entity.TipoPeriodicidade != TipoPeriodicidade.Nenhuma)
+                if (repetir)
                 {
                     for (int iRepeticao = 1; iRepeticao <= entity.NumeroRepeticoes; iRepeticao++)
                     {
@@ -141,10 +137,27 @@ namespace Fly01.Financeiro.BL
             base.Delete(entityToDelete);
         }
 
+        private static bool RepeticaoValida(ContaFinanceira entity)
+        {
+            const int limiteSemanal = 208;
+            const int limiteMensal = 48;
+            const int limiteAnual = 4;
+
+            entity.Fail(entity.Repetir && entity.TipoPeriodicidade == TipoPeriodicidade.Nenhuma, TipoPeriodicidadeInvalida);
+            entity.Fail(entity.Repetir && !entity.NumeroRepeticoes.HasValue, NumeroRepeticoesInvalido);
+
+            entity.Fail((entity.Repetir && entity.TipoPeriodicidade != TipoPeriodicidade.Nenhuma && entity.NumeroRepeticoes.HasValue) &&
+                        ((entity.TipoPeriodicidade == TipoPeriodicidade.Semanal && !(entity.NumeroRepeticoes.Value > 0 && entity.NumeroRepeticoes.Value <= limiteSemanal)) ||
+                         (entity.TipoPeriodicidade == TipoPeriodicidade.Mensal && !(entity.NumeroRepeticoes.Value > 0 && entity.NumeroRepeticoes.Value <= limiteMensal)) ||
+                         (entity.TipoPeriodicidade == TipoPeriodicidade.Anual && !(entity.NumeroRepeticoes.Value > 0 && entity.NumeroRepeticoes.Value <= limiteAnual)))
+                , RepeticoesInvalidas);
+
+            return entity.IsValid();
+        }
+
         public static Error RepeticoesInvalidas = new Error("Número de repetições inválido. Somente até 48 Meses (4 Anos / 208 Semanas).");
         public static Error AlteracaoCondicaoParcelamento = new Error("Não é permitido alterar a condição de parcelamento.");
         public static Error AlteracaoConfiguracaoRecorrencia = new Error("Não é permitido alterar as configurações de recorrência.");
-
         public static Error TipoPeriodicidadeInvalida = new Error("Periodicidade inválida", "tipoPeriodicidade");
         public static Error NumeroRepeticoesInvalido = new Error("Número de repetições inválido", "numeroRepeticoes");
     }

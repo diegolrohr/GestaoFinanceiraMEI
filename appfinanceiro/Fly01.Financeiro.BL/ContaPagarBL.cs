3d075e1c8aa5b2062a61381ca4d97466bd22c32e
luancruz <lcruz.silva@hotmail.com>
2018-05-28 12:18:37 -0300

    Envio Conta Financeira - Cliente Null

--- appfinanceiro/Fly01.Financeiro.BL/ContaPagarBL.cs
+++ appfinanceiro/Fly01.Financeiro.BL/ContaPagarBL.cs
@@ -28,22 +28,6 @@ namespace Fly01.Financeiro.BL
 
         public override void ValidaModel(ContaPagar entity)
         {
-            const int limiteSemanal = 208;
-            const int limiteMensal = 48;
-            const int limiteAnual = 4;
-
-            entity.PlataformaId = PlataformaUrl;
-            entity.UsuarioInclusao = AppUser;
-
-            entity.Fail(entity.Repetir && entity.TipoPeriodicidade == TipoPeriodicidade.Nenhuma, TipoPeriodicidadeInvalida);
-            entity.Fail(entity.Repetir && !entity.NumeroRepeticoes.HasValue, NumeroRepeticoesInvalido);
-
-            entity.Fail((entity.Repetir && entity.TipoPeriodicidade != TipoPeriodicidade.Nenhuma && entity.NumeroRepeticoes.HasValue) &&
-                ((entity.TipoPeriodicidade == TipoPeriodicidade.Semanal && !(entity.NumeroRepeticoes.Value > 0 && entity.NumeroRepeticoes.Value <= limiteSemanal)) ||
-                (entity.TipoPeriodicidade == TipoPeriodicidade.Mensal && !(entity.NumeroRepeticoes.Value > 0 && entity.NumeroRepeticoes.Value <= limiteMensal)) ||
-                (entity.TipoPeriodicidade == TipoPeriodicidade.Anual && !(entity.NumeroRepeticoes.Value > 0 && entity.NumeroRepeticoes.Value <= limiteAnual)))
-            , RepeticoesInvalidas);
-
             entity.Fail(entity.Numero < 1, NumeroContaInvalido);
             entity.Fail(Everything.Any(x => x.Numero == entity.Numero && x.Id != entity.Id), NumeroContaDuplicada);
             entity.Fail(entity.StatusContaBancaria == StatusContaBancaria.Pago && entity.ValorPrevisto != entity.ValorPago, StatusPagoSaldoInvalido);
@@ -55,6 +39,8 @@ namespace Fly01.Financeiro.BL
         {
             entity.PlataformaId = PlataformaUrl;
             entity.UsuarioInclusao = AppUser;
+
+            var repetir = RepeticaoValida(entity);
 
             //ContaFinanceira.Número
             var max = Everything.Any(x => x.Id != entity.Id) ? Everything.Max(x => x.Numero) : 0;
@@ -69,7 +55,7 @@ namespace Fly01.Financeiro.BL
                 entity.PessoaId = pessoaBL.BuscaPessoaNome(entity.NomePessoa, false, true);
 
             var condicoesParcelamento = condicaoParcelamentoBL.GetPrestacoes(entity.CondicaoParcelamentoId, entity.DataVencimento, entity.ValorPrevisto);
-            Guid contaFinanceiraPrincipal = entity.Id == default(Guid) ? Guid.NewGuid() : entity.Id;
+            var contaFinanceiraPrincipal = entity.Id == default(Guid) ? Guid.NewGuid() : entity.Id;
             for (int iParcela = 0; iParcela < condicoesParcelamento.Count(); iParcela++)
             {
                 var parcela = condicoesParcelamento[iParcela];
@@ -84,13 +70,22 @@ namespace Fly01.Financeiro.BL
                 itemContaPagar.DescricaoParcela = parcela.DescricaoParcela;
                 itemContaPagar.ValorPrevisto = parcela.Valor;
 
-                itemContaPagar.Id = iParcela == default(int) ? contaFinanceiraPrincipal : Guid.NewGuid();
+                if (iParcela == default(int))
+                {
+                    itemContaPagar.Id = contaFinanceiraPrincipal;
+                }
+                else
+                {
+                    itemContaPagar.Id = Guid.NewGuid();
+                    if (repetir)
+                        itemContaPagar.ContaFinanceiraRepeticaoPaiId = contaFinanceiraPrincipal;
+                }
 
                 itemContaPagar.Numero = ++max;
 
                 base.Insert(itemContaPagar);
 
-                if (entity.Repetir && entity.TipoPeriodicidade != TipoPeriodicidade.Nenhuma)
+                if (repetir)
                 {
                     for (int iRepeticao = 1; iRepeticao <= entity.NumeroRepeticoes; iRepeticao++)
                     {
@@ -149,13 +144,34 @@ namespace Fly01.Financeiro.BL
             base.Delete(entityToDelete);
         }
 
+        private static bool RepeticaoValida(ContaFinanceira entity)
+        {
+            if (entity.Repetir)
+            {
+                const int limiteSemanal = 208;
+                const int limiteMensal = 48;
+                const int limiteAnual = 4;
+
+                entity.Fail(entity.TipoPeriodicidade == TipoPeriodicidade.Nenhuma, TipoPeriodicidadeInvalida);
+                entity.Fail(!entity.NumeroRepeticoes.HasValue, NumeroRepeticoesInvalido);
+
+                entity.Fail((entity.TipoPeriodicidade != TipoPeriodicidade.Nenhuma && entity.NumeroRepeticoes.HasValue) &&
+                            ((entity.TipoPeriodicidade == TipoPeriodicidade.Semanal && !(entity.NumeroRepeticoes.Value > 0 && entity.NumeroRepeticoes.Value <= limiteSemanal)) ||
+                             (entity.TipoPeriodicidade == TipoPeriodicidade.Mensal && !(entity.NumeroRepeticoes.Value > 0 && entity.NumeroRepeticoes.Value <= limiteMensal)) ||
+                             (entity.TipoPeriodicidade == TipoPeriodicidade.Anual && !(entity.NumeroRepeticoes.Value > 0 && entity.NumeroRepeticoes.Value <= limiteAnual)))
+                    , RepeticoesInvalidas);
+
+                return entity.Repetir && entity.IsValid();
+            }
+
+            return false;
+        }
+
         public static Error RepeticoesInvalidas = new Error("Número de repetições inválido. Somente até 48 Meses (4 Anos / 208 Semanas).");
         public static Error AlteracaoCondicaoParcelamento = new Error("Não é permitido alterar a condição de parcelamento.");
         public static Error AlteracaoConfiguracaoRecorrencia = new Error("Não é permitido alterar as configurações de recorrência.");
-
         public static Error TipoPeriodicidadeInvalida = new Error("Periodicidade inválida", "tipoPeriodicidade");
         public static Error NumeroRepeticoesInvalido = new Error("Número de repetições inválido", "numeroRepeticoes");
-
         public static Error StatusPagoSaldoInvalido = new Error("Status Conta Bancária Paga, Valor Previsto deve ser igual a Valor Pago", "valorPrevisto");
         public static Error NumeroContaInvalido = new Error("Número da conta inválido", "numero");
         public static Error NumeroContaDuplicada = new Error("Número da conta duplicado", "numero");
--- appfinanceiro/Fly01.Financeiro.BL/ContaReceberBL.cs
+++ appfinanceiro/Fly01.Financeiro.BL/ContaReceberBL.cs
@@ -28,19 +28,6 @@ namespace Fly01.Financeiro.BL
 
         public override void ValidaModel(ContaReceber entity)
         {
-            const int limiteSemanal = 208;
-            const int limiteMensal = 48;
-            const int limiteAnual = 4;
-
-            entity.Fail(entity.Repetir && entity.TipoPeriodicidade == TipoPeriodicidade.Nenhuma, TipoPeriodicidadeInvalida);
-            entity.Fail(entity.Repetir && !entity.NumeroRepeticoes.HasValue, NumeroRepeticoesInvalido);
-
-            entity.Fail((entity.Repetir && entity.TipoPeriodicidade != TipoPeriodicidade.Nenhuma && entity.NumeroRepeticoes.HasValue) &&
-                ((entity.TipoPeriodicidade == TipoPeriodicidade.Semanal && !(entity.NumeroRepeticoes.Value > 0 && entity.NumeroRepeticoes.Value <= limiteSemanal)) ||
-                (entity.TipoPeriodicidade == TipoPeriodicidade.Mensal && !(entity.NumeroRepeticoes.Value > 0 && entity.NumeroRepeticoes.Value <= limiteMensal)) ||
-                (entity.TipoPeriodicidade == TipoPeriodicidade.Anual && !(entity.NumeroRepeticoes.Value > 0 && entity.NumeroRepeticoes.Value <= limiteAnual)))
-            , RepeticoesInvalidas);
-
             entity.Fail(entity.Numero < 1, NumeroContaInvalido);
             entity.Fail(Everything.Any(x => x.Numero == entity.Numero && x.Id != entity.Id), NumeroContaDuplicada);
             entity.Fail(entity.StatusContaBancaria == StatusContaBancaria.Pago && entity.ValorPrevisto != entity.ValorPago, StatusPagoSaldoInvalido);
@@ -52,6 +39,8 @@ namespace Fly01.Financeiro.BL
         {
             entity.PlataformaId = PlataformaUrl;
             entity.UsuarioInclusao = AppUser;
+
+            var repetir = RepeticaoValida(entity);
 
             //ContaFinanceira.Número
             var max = Everything.Any(x => x.Id != entity.Id) ? Everything.Max(x => x.Numero) : 0;
@@ -81,13 +70,22 @@ namespace Fly01.Financeiro.BL
                 itemContaReceber.DescricaoParcela = parcela.DescricaoParcela;
                 itemContaReceber.ValorPrevisto = parcela.Valor;
 
-                itemContaReceber.Id = iParcela == default(int) ? contaFinanceiraPrincipal : default(Guid);
+                if (iParcela == default(int))
+                {
+                    itemContaReceber.Id = contaFinanceiraPrincipal;
+                }
+                else
+                {
+                    itemContaReceber.Id = Guid.NewGuid();
+                    if (repetir)
+                        itemContaReceber.ContaFinanceiraRepeticaoPaiId = contaFinanceiraPrincipal;
+                }
 
                 itemContaReceber.Numero = ++max;
 
                 base.Insert(itemContaReceber);
 
-                if (entity.Repetir && entity.TipoPeriodicidade != TipoPeriodicidade.Nenhuma)
+                if (repetir)
                 {
                     for (int iRepeticao = 1; iRepeticao <= entity.NumeroRepeticoes; iRepeticao++)
                     {
@@ -145,13 +143,34 @@ namespace Fly01.Financeiro.BL
             base.Delete(entityToDelete);
         }
 
+        private static bool RepeticaoValida(ContaFinanceira entity)
+        {
+            if (entity.Repetir)
+            {
+                const int limiteSemanal = 208;
+                const int limiteMensal = 48;
+                const int limiteAnual = 4;
+
+                entity.Fail(entity.TipoPeriodicidade == TipoPeriodicidade.Nenhuma, TipoPeriodicidadeInvalida);
+                entity.Fail(!entity.NumeroRepeticoes.HasValue, NumeroRepeticoesInvalido);
+
+                entity.Fail((entity.TipoPeriodicidade != TipoPeriodicidade.Nenhuma && entity.NumeroRepeticoes.HasValue) &&
+                            ((entity.TipoPeriodicidade == TipoPeriodicidade.Semanal && !(entity.NumeroRepeticoes.Value > 0 && entity.NumeroRepeticoes.Value <= limiteSemanal)) ||
+                             (entity.TipoPeriodicidade == TipoPeriodicidade.Mensal && !(entity.NumeroRepeticoes.Value > 0 && entity.NumeroRepeticoes.Value <= limiteMensal)) ||
+                             (entity.TipoPeriodicidade == TipoPeriodicidade.Anual && !(entity.NumeroRepeticoes.Value > 0 && entity.NumeroRepeticoes.Value <= limiteAnual)))
+                    , RepeticoesInvalidas);
+
+                return entity.Repetir && entity.IsValid();
+            }
+
+            return false;
+        }
+
         public static Error RepeticoesInvalidas = new Error("Número de repetições inválido. Somente até 48 Meses (4 Anos / 208 Semanas).");
         public static Error AlteracaoCondicaoParcelamento = new Error("Não é permitido alterar a condição de parcelamento.");
         public static Error AlteracaoConfiguracaoRecorrencia = new Error("Não é permitido alterar as configurações de recorrência.");
-
         public static Error TipoPeriodicidadeInvalida = new Error("Periodicidade inválida", "tipoPeriodicidade");
         public static Error NumeroRepeticoesInvalido = new Error("Número de repetições inválido", "numeroRepeticoes");
-
         public static Error StatusPagoSaldoInvalido = new Error("Status Conta Bancária Paga, Valor Previsto deve ser igual a Valor Pago", "valorPrevisto");
         public static Error NumeroContaInvalido = new Error("Número da conta inválido", "numero");
         public static Error NumeroContaDuplicada = new Error("Número da conta duplicado", "numero");

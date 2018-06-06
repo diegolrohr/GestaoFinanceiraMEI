9953965084c04d8b16d8d770bff99625a8327f09
Jefferson Follmann <jeff.follmann@gmail.com>
2018-06-06 18:25:00 -0300

    Fix Messages

--- Fly01.Core.Mensageria/Slack/SlackField.cs
+++ Fly01.Core.Mensageria/Slack/SlackField.cs
@@ -6,14 +6,19 @@ namespace Fly01.Core.Mensageria.Slack
     {
         [JsonProperty("title")]
         public string Title { get; set; }
+
         [JsonProperty("value")]
         public string Value { get; set; }
 
+        [JsonProperty("short")]
+        public bool Short { get; set; }
+
         public SlackField() { }
-        public SlackField(string title, string value)
+        public SlackField(string title, string value, bool shortValue = false)
         {
             Title = title;
             Value = value;
+            Short = shortValue;
         }
     }
 }
--- Fly01.Core.Mensageria/Slack/SlackMessage.cs
+++ Fly01.Core.Mensageria/Slack/SlackMessage.cs
@@ -5,7 +5,15 @@ namespace Fly01.Core.Mensageria.Slack
 {
     public class SlackMessage
     {
+        [JsonProperty("icon_emoji")]
+        public string IconEmoji  { get; set; }
+
         [JsonProperty("attachments")]
         public List<SlackAttachment> Attachments { get; set; }
+
+        public SlackMessage()
+        {
+            IconEmoji = ":robot_face:";
+        }
     }
 }

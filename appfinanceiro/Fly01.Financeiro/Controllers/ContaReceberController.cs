4184689437179356af392e983c480e60dba8645f
Honatel <marcos.honatel@totvs.com.br>
2018-06-25 09:59:49 -0300

    Ajustes Cnab

--- appfinanceiro/Fly01.Financeiro/Controllers/ContaReceberController.cs
+++ appfinanceiro/Fly01.Financeiro/Controllers/ContaReceberController.cs
@@ -248,7 +248,7 @@ namespace Fly01.Financeiro.Controllers
             //new HtmlUIButton { Id = "gerararBoletosBtn", Label = "Gerar boleto"/*, OnClickFn = "fnModalContaBancaria" */},
             config.Actions.Add(new DataTableUIAction { OnClickFn = "fnEditar", Label = "Editar", ShowIf = "(row.statusEnum == 'EmAberto')" });
             config.Actions.Add(new DataTableUIAction { OnClickFn = "fnVisualizar", Label = "Visualizar" });
-            config.Actions.Add(new DataTableUIAction { OnClickFn = "", Label = "Gerar boleto" });
+          //  config.Actions.Add(new DataTableUIAction { OnClickFn = "", Label = "Gerar boleto" });
             config.Actions.Add(new DataTableUIAction { OnClickFn = "fnExcluir", Label = "Excluir", ShowIf = "(row.statusEnum == 'EmAberto' && row.repeticaoPai == false && row.repeticaoFilha == false)" });
             config.Actions.Add(new DataTableUIAction { OnClickFn = "fnExcluirRecorrencias", Label = "Excluir", ShowIf = "(row.statusEnum == 'EmAberto' && (row.repeticaoPai == true || row.repeticaoFilha == true))" });
             config.Actions.Add(new DataTableUIAction { OnClickFn = "fnNovaBaixa", Label = "Nova baixa", ShowIf = "row.statusEnum == 'EmAberto' || row.statusEnum == 'BaixadoParcialmente'" });
@@ -712,37 +712,37 @@ namespace Fly01.Financeiro.Controllers
             return base.Delete(id);
         }
 
-        public ContentResult ModalContaBancaria()
-        {
-            ModalUIForm config = new ModalUIForm()
-            {
-                Title = "Selecione a conta bancária para gerar o boleto",
-                ConfirmAction = new ModalUIAction() { Label = "Gerar boleto" , OnClickFn = "fnImprimirBoleto" },
-                CancelAction = new ModalUIAction() { Label = "Cancelar" },
-                Action = new FormUIAction
-                {
-                    Create = "", // @Url.Action("Create"),
-                    Edit = "", //@Url.Action("Edit"),
-                    Get = "", //@Url.Action("Json") + "/",
-                },
-                Id = "fly01mdlfrmModalCliente",
-                UrlFunctions = Url.Action("Functions") + "?fns="
-            };
+        //public ContentResult ModalContaBancaria()
+        //{
+        //    ModalUIForm config = new ModalUIForm()
+        //    {
+        //        Title = "Selecione a conta bancária para gerar o boleto",
+        //        ConfirmAction = new ModalUIAction() { Label = "Gerar boleto" , OnClickFn = "fnImprimirBoleto" },
+        //        CancelAction = new ModalUIAction() { Label = "Cancelar" },
+        //        Action = new FormUIAction
+        //        {
+        //            Create = "", // @Url.Action("Create"),
+        //            Edit = "", //@Url.Action("Edit"),
+        //            Get = "", //@Url.Action("Json") + "/",
+        //        },
+        //        Id = "fly01mdlfrmModalCliente",
+        //        UrlFunctions = Url.Action("Functions") + "?fns="
+        //    };
 
 
-            config.Elements.Add(new InputHiddenUI { Id = "Id" });
-            config.Elements.Add(new AutoCompleteUI
-            {
-                Id = "bancoId",
-                Class = "col s12 m6 l6",
-                Label = "Banco cedente",
-                Required = true,
-                DataUrl = @Url.Action("ContaBancariaBancoEmiteBoleto", "AutoComplete") + "?emiteBoleto=true",
-                LabelId = "bancoNome"
-            });
+        //    config.Elements.Add(new InputHiddenUI { Id = "Id" });
+        //    config.Elements.Add(new AutoCompleteUI
+        //    {
+        //        Id = "bancoId",
+        //        Class = "col s12 m6 l6",
+        //        Label = "Banco cedente",
+        //        Required = true,
+        //        DataUrl = @Url.Action("ContaBancariaBancoEmiteBoleto", "AutoComplete") + "?emiteBoleto=true",
+        //        LabelId = "bancoNome"
+        //    });
 
-            return Content(JsonConvert.SerializeObject(config, JsonSerializerSetting.Front), "application/json");
-        }
+        //    return Content(JsonConvert.SerializeObject(config, JsonSerializerSetting.Front), "application/json");
+        //}
 
         #region OnDemmand
 

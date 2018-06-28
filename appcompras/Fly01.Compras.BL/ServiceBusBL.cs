2f11ff6c6e3954d24b7243ef341c3a3611e71d72
israel.borba <israel.borba@totvs.com.br>
2018-06-28 12:05:48 -0300

    Unificação de código da ServiceBusBL na classe base ServiceBusBase

--- appcompras/Fly01.Compras.BL/ServiceBusBL.cs
+++ appcompras/Fly01.Compras.BL/ServiceBusBL.cs
@@ -1,52 +1,11 @@
 ﻿using System;
-using Fly01.Core;
-using Fly01.Core.Base;
 using System.Reflection;
 using Fly01.Core.ServiceBus;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-using Newtonsoft.Json;
 
 namespace Fly01.Compras.BL
 {
-    public class ServiceBusBL : Consumer
+    public class ServiceBusBL : ServiceBusBase
     {
-        const string domainAssemblyName = "Fly01.Core.Entities";
-
-        public ServiceBusBL()
-        {
-            SetupEnvironment.Create();
-
-            base.Consume();
-        }
-
-        protected override async Task PersistMessage()
-        {
-            RabbitConfig.RoutingKey = "Categoria"; //hardcode para testes
-            var unitOfWorkAssembly = Type.GetType(Assembly.GetExecutingAssembly().GetName().Name + ".UnitOfWork");
-            var domainAssembly = Assembly.Load(domainAssemblyName).GetType($"{domainAssemblyName}.Domains.Commons.{RabbitConfig.RoutingKey}");
-            dynamic entidade = unitOfWorkAssembly.GetProperty(RabbitConfig.RoutingKey + "BL")?.GetGetMethod(false);
-
-            exceptions = new List<KeyValuePair<string, object>>();
-
-            using (var unitOfWork = new UnitOfWork(new ContextInitialize() { AppUser = RabbitConfig.AppUser, PlataformaUrl = RabbitConfig.PlataformaUrl }))
-            {
-                foreach (var item in MessageType.Resolve<dynamic>(Message))
-                {
-                    try
-                    {
-                        var data = JsonConvert.DeserializeObject(item.ToString(), domainAssembly);
-
-                        entidade?.Invoke(unitOfWork, null).PersistMessage(data, HTTPMethod);
-                        await unitOfWork.Save();
-                    }
-                    catch (Exception ex)
-                    {
-                        exceptions.Add(new KeyValuePair<string, object>(item.ToString(), ex));
-                        continue;
-                    }
-                }
-            }
-        }
+        public ServiceBusBL() : base(Type.GetType($"{Assembly.GetExecutingAssembly().GetName().Name}.UnitOfWork")) { }
     }
 }

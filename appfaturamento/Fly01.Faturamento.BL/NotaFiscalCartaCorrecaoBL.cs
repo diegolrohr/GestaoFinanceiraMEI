3017b62416586ca3b2671a2e284be64a65684af6
Cássio Tomilin <cassio.tomilin@totvs.com.br>
2018-07-06 09:39:38 -0300

    BL
    BL

---
+++ appfaturamento/Fly01.Faturamento.BL/NotaFiscalCartaCorrecaoBL.cs
@@ -0,0 +1,20 @@
+﻿using Fly01.Core.BL;
+using Fly01.Core.Entities.Domains.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fly01.Faturamento.BL
+{
+    public class NotaFiscalCartaCorrecaoBL : PlataformaBaseBL<NotaFiscalCartaCorrecao>
+    {
+
+        protected NotaFiscalBL NotaFiscalBL { get; set; }
+        public NotaFiscalCartaCorrecaoBL(AppDataContextBase context, NotaFiscalBL notaFiscalBL) : base(context)
+        {
+            NotaFiscalBL = notaFiscalBL;
+        }
+    }
+}

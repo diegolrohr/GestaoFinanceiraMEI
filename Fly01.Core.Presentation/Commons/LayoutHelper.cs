1547542f79ebb9de49b183dee219ed1b99186d3f
Alexis Diel <alexisdiel@gmail.com>
2018-07-18 11:52:20 -0300

    versao minificada

--- Fly01.Core.Presentation/Commons/LayoutHelper.cs
+++ Fly01.Core.Presentation/Commons/LayoutHelper.cs
@@ -7,8 +7,13 @@ namespace Fly01.Core.Presentation.Commons
     {
         private static string cdnVersion = "1.1.4";
 
-        private static string serverUrl = $"https://cdnfly01.azureedge.net/fly/{cdnVersion}"; /*
-        private static string serverUrl = $"http://poad257.poa01.local:8000/{cdnVersion}"; /**/
+        private static string serverUrl = $"https://cdnfly01.azureedge.net/fly/{cdnVersion}"; private static string complJs = ".min"; /*
+        private static string serverUrl = $"http://poad257.poa01.local:8000/{cdnVersion}";
+        private static string complJs = ""; /**/
+
+        private static string cssUrl = $"{serverUrl}/fly01ui.css";
+        private static string jsUrl = $"{serverUrl}/fly01ui{complJs}.js";
+
 
         public static IHtmlString HeadTemplate(this HtmlHelper<dynamic> html, string appName)
         {
@@ -17,13 +22,13 @@ namespace Fly01.Core.Presentation.Commons
                 "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0\">" +
                 $"<title>{appName}</title>" +
                 "<base href=\"~/\" />" +
-                $"<link type=\"text/css\" rel=\"stylesheet\" href=\"{serverUrl}/fly01ui.css\" />"
+                $"<link type=\"text/css\" rel=\"stylesheet\" href=\"{cssUrl}\" />"
             );
         }
 
         public static IHtmlString ScriptsTemplate(this HtmlHelper html)
         {
-            return new HtmlString($"<script src=\"{serverUrl}/fly01ui.js\"></script>");
+            return new HtmlString($"<script src=\"{jsUrl}\"></script>");
         }
     }
 }

4a898cc88d8615cffd0c8896dfdca97b5fcb4dce
israel.borba <israel.borba@totvs.com.br>
2018-07-26 17:28:14 -0300

    Correção validação de cep

--- appfinanceiro/Fly01.Financeiro.BL/PessoaBL.cs
+++ appfinanceiro/Fly01.Financeiro.BL/PessoaBL.cs
@@ -97,13 +97,9 @@ namespace Fly01.Financeiro.BL
         {
             if (!string.IsNullOrEmpty(entity.CEP))
             {
-                int cep;
+                entity.CEP = entity.CEP.Replace("-", "");
 
-                if (entity.CEP.Length == 9 && entity.CEP.Substring(5, 1) == "-")
-                    entity.CEP = entity.CEP.Replace("-", "");
-
-                if (entity.CEP.Length != 8 || !int.TryParse(entity.CEP, out cep) || cep == 0)
-                    entity.Fail(true, FormatoCepInvalido);
+                entity.Fail(entity.CEP.Length != 8, FormatoCepInvalido);
             }
         }
 

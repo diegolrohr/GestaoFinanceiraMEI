859edd441f78def07c21ed65879e469085bc1087
Honatel <marcos.honatel@totvs.com.br>
2018-08-13 18:02:40 -0300

    Ajustes  Fix

--- appemissaoNFE/Fly01.EmissaoNFE.BL/SubstituicaoTributariaBL.cs
+++ appemissaoNFE/Fly01.EmissaoNFE.BL/SubstituicaoTributariaBL.cs
@@ -12,21 +12,51 @@ namespace Fly01.EmissaoNFE.BL
         {
         }
 
-        public TributacaoRetornoBaseVM SubstituicaoTributaria(Tributacao entity, TabelaIcmsBL TabelaIcmsBL)
+        public TributacaoRetornoBaseVM CalculaSubstituicaoTributaria(Tributacao entity, TabelaIcmsBL TabelaIcmsBL)
         {
-            entity.SubstituicaoTributaria.Base = entity.SubstituicaoTributaria.IpiNaBase ? entity.ValorBase + entity.Ipi.Valor : entity.ValorBase;
-            entity.SubstituicaoTributaria.Base += entity.SubstituicaoTributaria.DespesaNaBase ? entity.ValorDespesa : 0;
-            entity.SubstituicaoTributaria.Base += entity.SubstituicaoTributaria.FreteNaBase ? entity.ValorFrete : 0;
-            
-            entity.SubstituicaoTributaria.Aliquota = (from e in TabelaIcmsBL.All
-                               where e.SiglaDestino.Equals(entity.SubstituicaoTributaria.EstadoDestino, StringComparison.InvariantCultureIgnoreCase)
-                               && e.SiglaOrigem.Equals(entity.SubstituicaoTributaria.EstadoOrigem, StringComparison.InvariantCultureIgnoreCase)
-                               select e.IcmsAliquota).FirstOrDefault();
+            var valorBase = ObterValorBase(entity);
 
-            entity.SubstituicaoTributaria.Base = (100 + entity.SubstituicaoTributaria.Mva) / 100 * entity.SubstituicaoTributaria.Base;
-            entity.SubstituicaoTributaria.Valor = Math.Round(entity.SubstituicaoTributaria.Base / 100 * entity.SubstituicaoTributaria.Aliquota, 2);
-            
+            var aliquotaIntraEstadual = ObterAliquotaIntraEstadual(entity, TabelaIcmsBL);
+            var aliquotaInterEstadual = ObterAliquotaInterEstadual(entity, TabelaIcmsBL);
+
+            entity.SubstituicaoTributaria.Aliquota = aliquotaIntraEstadual;
+            entity.SubstituicaoTributaria.Base = (100 + entity.SubstituicaoTributaria.Mva) / 100 * valorBase;
+
+            var valorIntraEstadual = Math.Round(entity.SubstituicaoTributaria.Base / 100 * aliquotaIntraEstadual, 2);
+            var valorInterEstadual = Math.Round(valorBase / 100 * aliquotaInterEstadual, 2);
+
+            entity.SubstituicaoTributaria.Valor = valorIntraEstadual - valorInterEstadual;
+
             return new TributacaoRetornoBaseVM { Base = entity.SubstituicaoTributaria.Base, Aliquota = entity.SubstituicaoTributaria.Aliquota, Valor = entity.SubstituicaoTributaria.Valor, AgregaTotalNota = true };
+        }
+
+        private double ObterValorBase(Tributacao entity)
+        {
+            var valorBase = entity.SubstituicaoTributaria.IpiNaBase ? entity.ValorBase + entity.Ipi.Valor : entity.ValorBase;
+            valorBase += entity.SubstituicaoTributaria.DespesaNaBase ? entity.ValorDespesa : 0;
+            valorBase += entity.SubstituicaoTributaria.FreteNaBase ? entity.ValorFrete : 0;
+
+            return valorBase;
+        }
+
+        private double ObterAliquotaIntraEstadual(Tributacao entity, TabelaIcmsBL TabelaIcmsBL)
+        {
+            return ObterAliquota(entity.SubstituicaoTributaria.EstadoOrigem, entity.SubstituicaoTributaria.EstadoOrigem, TabelaIcmsBL);
+        }
+
+        private double ObterAliquotaInterEstadual(Tributacao entity, TabelaIcmsBL TabelaIcmsBL)
+        {
+            return entity.SubstituicaoTributaria.EstadoOrigem != entity.SubstituicaoTributaria.EstadoDestino 
+                ? ObterAliquota(entity.SubstituicaoTributaria.EstadoOrigem, entity.SubstituicaoTributaria.EstadoDestino, TabelaIcmsBL)
+                 :0.0;
+        }
+
+        private double ObterAliquota(string EstadoOrigem, string EstadoDestino, TabelaIcmsBL TabelaIcmsBL)
+        {
+            return  (from e in TabelaIcmsBL.All
+                        where e.SiglaDestino.Equals(EstadoOrigem, StringComparison.InvariantCultureIgnoreCase)
+                        && e.SiglaOrigem.Equals(EstadoOrigem, StringComparison.InvariantCultureIgnoreCase)
+                        select e.IcmsAliquota).FirstOrDefault();
         }
     }
 }

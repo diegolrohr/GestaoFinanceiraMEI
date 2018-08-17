5feff59e0667a49d3e3801eb6331840ab7f1eb8c
patricia.oshiro <patricia.oshiro@totvs.com.br>
2018-08-17 12:46:05 -0300

    Inclusão de BLs e Controllers dentro de OrdemServico

---
+++ appordemServico/Fly01.OrdemServico.API/Controllers/Api/EstadoController.cs
@@ -0,0 +1,12 @@
+﻿using Fly01.Core.Entities.Domains.Commons;
+using Fly01.OrdemServico.BL;
+using System.Web.OData.Routing;
+
+namespace Fly01.OrdemServico.API.Controllers.Api
+{
+    [ODataRoutePrefix("estado")]
+    public class EstadoController : ApiDomainController<Estado, EstadoBL>
+    {
+
+    }
+}
---
+++ appordemServico/Fly01.OrdemServico.BL/ParametroOrdemServicoBL.cs
@@ -0,0 +1,14 @@
+﻿using Fly01.Core.BL;
+using Fly01.Core.Entities.Domains.Commons;
+using Fly01.OrdemServico.API.Models.DAL;
+
+namespace Fly01.OrdemServico.BL
+{
+    class ParametroOrdemServicoBL : PlataformaBaseBL<ParametroOrdemServico>
+    {
+        public ParametroOrdemServicoBL(AppDataContext context) : base(context)
+        {
+
+        }
+    }
+}

1866a1f5543d2e28239242f2359ceb878b56870a
Thiago Oliveira Santos <thiago.santos@totvs.com.br>
2018-08-21 17:22:28 -0300

    Inclusão de Controller e Bl de NBS

---
+++ appordemServico/Fly01.OrdemServico.BL/NBSBL.cs
@@ -0,0 +1,11 @@
+﻿using Fly01.Core.BL;
+using Fly01.Core.Entities.Domains.Commons;
+using Fly01.OrdemServico.API.Models.DAL;
+
+namespace Fly01.OrdemServico.BL
+{
+    public class NBSBL : DomainBaseBL<Nbs>
+    {
+        public NBSBL(AppDataContext context) : base(context) { }
+    }
+}

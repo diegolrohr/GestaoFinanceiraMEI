53fae4b1321cd7e26b742200621c192ecc70f91c
Cássio Tomilin <cassio.tomilin@totvs.com.br>
2018-08-21 10:56:35 -0300

    Visualizar pedido

--- appcompras/Fly01.Compras/ViewModel/PedidoItemVM.cs
+++ appcompras/Fly01.Compras/ViewModel/PedidoItemVM.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Fly01.Core.ViewModels.Presentation.Commons;
+using Newtonsoft.Json;
 using System;
 
 namespace Fly01.Compras.ViewModel
@@ -7,7 +8,28 @@ namespace Fly01.Compras.ViewModel
     {
         [JsonProperty("pedidoId")]
         public Guid PedidoId { get; set; }
-        
+
+        [JsonProperty("valorCreditoICMS")]
+        public double ValorCreditoICMS { get; set; }
+
+        [JsonProperty("valorICMSSTRetido")]
+        public double ValorICMSSTRetido { get; set; }
+
+        [JsonProperty("valorBCSTRetido")]
+        public double ValorBCSTRetido { get; set; }
+
+        [JsonProperty("valorFCPSTRetidoAnterior")]
+        public double ValorFCPSTRetidoAnterior { get; set; }
+
+        [JsonProperty("valorBCFCPSTRetidoAnterior")]
+        public double ValorBCFCPSTRetidoAnterior { get; set; }
+
+        [JsonProperty("grupoTributarioId")]
+        public Guid GrupoTributarioId { get; set; }
+
+        [JsonProperty("grupoTributario")]
+        public virtual GrupoTributarioVM GrupoTributario { get; set; }
+
         [JsonProperty("pedido")]
         public virtual PedidoVM Pedido { get; set; }
     }

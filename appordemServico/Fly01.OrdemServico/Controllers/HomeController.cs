b020c7730947a7910d0c8b1a3033c152dc98bdea
Maite Polydoro <waylandsarts@live.com>
2018-08-22 15:05:06 -0300

    ajustes home dashboard

--- appordemServico/Fly01.OrdemServico/Controllers/HomeController.cs
+++ appordemServico/Fly01.OrdemServico/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using Fly01.Core.Config;
 using Fly01.Core.Presentation;
+using Fly01.Core.Presentation.Commons;
 using Fly01.uiJS.Classes;
+using Fly01.uiJS.Classes.Elements;
 using Fly01.uiJS.Classes.Widgets;
 using Fly01.uiJS.Defaults;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Web.Mvc;
@@ -23,18 +26,171 @@ namespace Fly01.OrdemServico.Controllers
             var cfg = new ContentUI
             {
                 History = new ContentUIHistory { Default = Url.Action("Index") },
-                //    //Header = new HtmlUIHeader
-                //    //{
-                //    //    Title = "Fluxo de Caixa",
-                //    //    Buttons = new List<HtmlUIButton>
-                //    //    {
-                //    //        new HtmlUIButton { Id = "save", Label = "Atualizar", OnClickFn = "fnAtualizar", Position = HtmlUIButtonPosition.Main },
-                //    //        new HtmlUIButton { Id = "prnt", Label = "Imprimir", OnClickFn = "fnImprimirFluxoCaixa", Position = HtmlUIButtonPosition.Out }
-                //    //    }
-                //    //},
-                //    //UrlFunctions = Url.Action("Functions", "Home", null, Request.Url.Scheme) + "?fns=",
-                //    //Functions = new List<string> { "__format", "fnGetSaldos" }
+                SidebarUrl = Url.Action("Sidebar"),
+                Header = new HtmlUIHeader()
+                {
+                    Title = "Visão Geral",
+                    Buttons = new List<HtmlUIButton>()
+                },
+                UrlFunctions = Url.Action("Functions", "Home", null, Request.Url.Scheme) + "?fns="
             };
+            var dataInicialFiltroDefault = DateTime.Now.Date;
+
+            var dataFinal = DateTime.Now.AddMonths(1);
+            var dataFinalFiltroDefault = new DateTime(dataFinal.Year, dataFinal.Month, DateTime.DaysInMonth(dataFinal.Year, dataFinal.Month));
+
+            cfg.Content.Add(new FormUI
+            {
+                ReadyFn = "fnFormReady",
+                UrlFunctions = Url.Action("Functions", "Home", null, Request.Url.Scheme) + "?fns=",
+                Class = "col s12",
+                Elements = new List<BaseUI>
+                {
+                    new InputDateUI
+                    {
+                        Id = "dataInicial",
+                        Class = "col s6 m3 l4 offset-l2 offset-m3",
+                        Label = "Data Inicial",
+                        Value = dataInicialFiltroDefault.ToString("dd/MM/yyyy"),
+                        DomEvents = new List<DomEventUI>
+                        {
+                            new DomEventUI {DomEvent = "click", Function = "fnAtualizar"}
+                        },
+                        Max = 90,
+                        Min = true
+                    },
+                    new InputDateUI
+                    {
+                        Id = "dataFinal",
+                        Class = "col s6 m3 l4",
+                        Label = "Data Final",
+                        Value = dataFinalFiltroDefault.ToString("dd/MM/yyyy"),
+                        DomEvents = new List<DomEventUI> {new DomEventUI {DomEvent = "click", Function = "fnAtualizar"}},
+                        Max = 90,
+                        Min = true
+                    },
+                    new InputHiddenUI{ Id = "groupType", Value = "1" },
+                    new ButtonGroupUI()
+                    {
+                        Id = "fly01btngrp",
+                        Class = "col s12 hide-on-print",
+                        Label = "Tipo de Visualização",
+                        OnClickFn = "fnAtualizaAgrupamento",
+                        Options = new List<ButtonGroupOptionUI>
+                        {
+                            new ButtonGroupOptionUI { Id = "btnDia", Value = "1", Label = "Dia", Class = "col s4 m2" },
+                            new ButtonGroupOptionUI { Id = "btnMes", Value = "3", Label = "Mês", Class = "col s4 m2" },
+                            new ButtonGroupOptionUI { Id = "btnTri", Value = "4", Label = "Trimestre", Class = "col s6 m3" },
+                            new ButtonGroupOptionUI { Id = "btnSem", Value = "5", Label = "Semestre", Class = "col s6 m3" },
+                            new ButtonGroupOptionUI { Id = "btnAno", Value = "6", Label = "Ano", Class = "col s4 m2" },
+                        }
+                    }
+                }
+            });
+
+            cfg.Content.Add(new CardUI
+            {
+                Class = "col s12 m3",
+                Color = "totvs-blue",
+                Id = "cardAberto",
+                Title = "Em Aberto",
+                Placeholder = "0/100",
+                Action = new LinkUI
+                {
+                    Label = "Ver mais",
+                    OnClick = @Url.Action("List", "OrdemServico")
+                }
+            });
+            cfg.Content.Add(new CardUI
+            {
+                Class = "col s12 m3",
+                Color = "red",
+                Id = "cardAndamento",
+                Title = "Em Andamento",
+                Placeholder = "0/100",
+                Action = new LinkUI
+                {
+                    Label = "Ver mais",
+                    OnClick = @Url.Action("List", "OrdemServico")
+                }
+            });
+            cfg.Content.Add(new CardUI
+            {
+                Class = "col s12 m3",
+                Color = "green",
+                Id = "cardConcluido",
+                Title = "Concluído",
+                Placeholder = "0/100",
+                Action = new LinkUI
+                {
+                    Label = "Ver mais",
+                    OnClick = @Url.Action("List", "OrdemServico")
+                }
+            });
+            cfg.Content.Add(new CardUI
+            {
+                Class = "col s12 m3",
+                Color = "teal",
+                Id = "cardCancelado",
+                Title = "Cancelado",
+                Placeholder = "0/100",
+                Action = new LinkUI
+                {
+                    Label = "",
+                    OnClick = ""
+                }
+            });
+
+            cfg.Content.Add(new ChartUI
+            {
+                Options = new
+                {
+                    title = new
+                    {
+                        display = true,
+                        text = "Ordens de Serviço por dia",
+                        fontSize = 12,
+                        fontFamily = "Roboto",
+                        fontColor = "#555"
+                    },
+                    tooltips = new
+                    {
+                        mode = "label",
+                        bodySpacing = 10,
+                        cornerRadius = 0,
+                        titleMarginBottom = 15
+                    },
+                    legend = new { position = "bottom" },
+                    global = new
+                    {
+                        responsive = false,
+                        maintainAspectRatio = false
+                    },
+                    scales = new
+                    {
+                        xAxes = new object[] {
+                                new
+                                {
+                                    stacked = true
+                                }
+                            },
+                        yAxes = new object[] {
+                                new
+                                {
+                                    stacked = true
+                                }
+                            }
+                    }
+                },
+                UrlData = @Url.Action("LoadChart", "Home"),
+                Class = "col s12",
+                Parameters = new List<ChartUIParameter>
+                {
+                    new ChartUIParameter { Id = "dataInicial" },
+                    new ChartUIParameter { Id = "dataFinal" },
+                    new ChartUIParameter { Id = "groupType" }
+                }
+            });
 
             if (withSidebarUrl)
                 cfg.SidebarUrl = Url.Action("Sidebar", "Home", null, Request.Url.Scheme);
@@ -122,5 +278,53 @@ namespace Fly01.OrdemServico.Controllers
 
             return Content(JsonConvert.SerializeObject(config, JsonSerializerSetting.Front), "application/json");
         }
+
+        public JsonResult LoadChart(DateTime dataInicial, DateTime dataFinal, int groupType)
+        {
+            try
+            {
+                //var response = GetProjecao(dataInicial, dataFinal, groupType);
+
+                var dataChartToView = new
+                {
+                    success = true,
+                    currency = true,
+                    //labels = response.Select(x => x.Label).ToArray(),
+                    //datasets = new object[] {
+                    //    new {
+                    //            type = "line",
+                    //            label = "Saldo",
+                    //            backgroundColor = "rgb(44, 55, 57)",
+                    //            borderColor = "rgb(44, 55, 57)",
+                    //            //data = response.Select(x => Math.Round(x.SaldoFinal, 2)).ToArray(),
+                    //            fill = false
+                    //        },
+                    //    new {
+                    //            label = "Recebimentos",
+                    //            fill = false,
+                    //            backgroundColor = "rgb(0, 178, 121)",
+                    //            borderColor = "rgb(0, 178, 121)",
+                    //            //data = response.Select(x => Math.Round(x.TotalRecebimentos, 2)).ToArray()
+                    //        },
+                    //    new {
+                    //            label = "Pagamentos",
+                    //            fill = false,
+                    //            backgroundColor = "rgb(239, 100, 97)",
+                    //            borderColor = "rgb(239, 100, 97)",
+                    //            //data = response.Select(x => Math.Round(x.TotalPagamentos * -1, 2)).ToArray()
+                    //    }
+                    //}
+                };
+
+                return Json(dataChartToView, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return JsonResponseStatus.GetFailure(ex.Message);
+            }
+        }
+
+
     }
+
 }

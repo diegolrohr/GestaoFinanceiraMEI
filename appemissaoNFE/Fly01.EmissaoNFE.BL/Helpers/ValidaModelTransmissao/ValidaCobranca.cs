6f6514acc8bed4086e079395b3154a3fafa7f1a2
diego.rohr <diego.rohr@totvs.com.br>
2018-08-22 09:32:06 -0300

    fix validaCobranca

--- appemissaoNFE/Fly01.EmissaoNFE.BL/Helpers/ValidaModelTransmissao/ValidaCobranca.cs
+++ appemissaoNFE/Fly01.EmissaoNFE.BL/Helpers/ValidaModelTransmissao/ValidaCobranca.cs
@@ -13,10 +13,8 @@ namespace Fly01.EmissaoNFE.BL.Helpers.ValidaModelTransmissao
 
             if (item.Cobranca != null && item.Cobranca.Fatura == null)
                 entity.Fail(true, new Error("Informe os dados da fatura. Item: " + nItem, "Item.Cobranca.Fatura"));
-            else
-            {
+            else if(item.Cobranca != null && item.Cobranca.Fatura != null)
                 ValidarDadosFatura(item, entity, nItem);
-            }
 
             #endregion Validação da classe Totais
         }

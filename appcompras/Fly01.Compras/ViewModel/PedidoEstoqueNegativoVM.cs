b0df5a695175c1db141c4bc70a6eed0f879997b7
Cássio Tomilin <cassio.tomilin@totvs.com.br>
2018-08-27 10:55:30 -0300

    Movimenta estoque
    Movimenta estoque

---
+++ appcompras/Fly01.Compras/ViewModel/PedidoEstoqueNegativoVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Fly01.Compras.ViewModel
+{
+    [Serializable]
+    public class PedidoEstoqueNegativoVM
+    {
+        [JsonProperty("produtoId")]
+        public Guid ProdutoId { get; set; }
+
+        [JsonProperty("quantPedido")]
+        public double QuantPedido { get; set; }
+
+        [JsonProperty("quantEstoque")]
+        public double QuantEstoque { get; set; }
+
+        [JsonProperty("saldoEstoque")]
+        public double SaldoEstoque { get; set; }
+
+        [JsonProperty("produtoDescricao")]
+        public string ProdutoDescricao { get; set; }
+    }
+}

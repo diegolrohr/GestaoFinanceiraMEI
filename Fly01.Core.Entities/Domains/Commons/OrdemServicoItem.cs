6797aa0bb6d5460c6a5b1514be2d043ee5b5003d
Thiago Oliveira Santos <thiago.santos@totvs.com.br>
2018-09-05 10:55:46 -0300

    Correção do cálculo do total

--- Fly01.Core.Entities/Domains/Commons/OrdemServicoItem.cs
+++ Fly01.Core.Entities/Domains/Commons/OrdemServicoItem.cs
@@ -10,6 +10,6 @@ namespace Fly01.Core.Entities.Domains.Commons
         public double Desconto { get; set; }
 
         [NotMapped]
-        public double Total => Quantidade > 0 ? Math.Round(((Quantidade * Valor) - Desconto), 2, MidpointRounding.AwayFromZero) : Math.Round((Valor - Desconto), 2, MidpointRounding.AwayFromZero);
+        public double Total => Math.Round(((Quantidade * Valor) - Desconto), 2, MidpointRounding.AwayFromZero);
     }
 }

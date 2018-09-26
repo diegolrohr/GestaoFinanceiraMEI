bfe699aab473242f890610b300ceb42d067127a4
diego.rohr <diego.rohr@totvs.com.br>
2018-09-26 17:54:45 -0300

    tributacao novas bls

---
+++ appemissaoNFE/Fly01.EmissaoNFE.BL/CofinsBL.cs
@@ -0,0 +1,25 @@
+﻿using Fly01.EmissaoNFE.Domain;
+using Fly01.EmissaoNFE.Domain.ViewModel;
+using System;
+using Fly01.Core.BL;
+
+namespace Fly01.EmissaoNFE.BL
+{
+    public class CofinsBL
+    {
+        public CofinsBL(AppDataContextBase context)
+        {
+        }
+        
+        public TributacaoRetornoBaseVM Cofins(Tributacao entity)
+        {            
+            return new TributacaoRetornoBaseVM
+            {
+                Base = entity.ValorBase,
+                Aliquota = entity.Cofins.Aliquota,
+                Valor = Math.Round(entity.ValorBase / 100 * entity.Cofins.Aliquota, 2),
+                AgregaTotalNota = false
+            };
+        }
+    }
+}
---
+++ appemissaoNFE/Fly01.EmissaoNFE.BL/PisBL.cs
@@ -0,0 +1,25 @@
+﻿using Fly01.EmissaoNFE.Domain;
+using Fly01.EmissaoNFE.Domain.ViewModel;
+using System;
+using Fly01.Core.BL;
+
+namespace Fly01.EmissaoNFE.BL
+{
+    public class PisBL
+    {
+        public PisBL(AppDataContextBase context)
+        {
+        }
+        
+        public TributacaoRetornoBaseVM Pis(Tributacao entity)
+        {            
+            return new TributacaoRetornoBaseVM
+            {
+                Base = entity.ValorBase,
+                Aliquota = entity.Pis.Aliquota,
+                Valor = Math.Round(entity.ValorBase / 100 * entity.Pis.Aliquota, 2),
+                AgregaTotalNota = false
+            };
+        }
+    }
+}

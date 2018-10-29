cd29519d7f398119b3d476076decacc31ecd7cdb
Honatel <marcos.honatel@totvs.com.br>
2018-10-29 16:22:19 -0300

    Refatoração OKR

--- appcompras/Fly01.Compras.BL/MonitorNFBL.cs
+++ appcompras/Fly01.Compras.BL/MonitorNFBL.cs
@@ -37,11 +37,7 @@ namespace Fly01.Compras.BL
                                             group nf by nf.PlataformaId into g
                                             select new { plataformaId = g.Key, notaInicial = g.Min(x => x.SefazId), notaFinal = g.Max(x => x.SefazId) });
 
-            var header = new Dictionary<string, string>()
-            {
-                { "AppUser", AppUser },
-                { "PlataformaUrl", string.IsNullOrEmpty(plataformaUrl) ? PlataformaUrl : plataformaUrl }
-            };
+            var header = GetHeader(PlataformaUrl);
 
             foreach (var dadosPlataforma in notasFiscaisByPlataforma)
             {
@@ -52,37 +48,15 @@ namespace Fly01.Compras.BL
                     if (dadosCertificado == null)
                         continue;
 
-
                     if (TotalTributacaoBL.ConfiguracaoTSSOK(dadosPlataforma.plataformaId))
                     {
-                        var monitorVM = new MonitorVM()
-                        {
-                            Homologacao = dadosCertificado.Homologacao,
-                            Producao = dadosCertificado.Producao,
-                            EntidadeAmbiente = dadosCertificado.EntidadeAmbiente,
-                            NotaInicial = dadosPlataforma.notaInicial.ToString(),
-                            NotaFinal = dadosPlataforma.notaFinal.ToString(),
-                        };
+                        var monitorVM = GetMonitorVM(dadosCertificado, dadosPlataforma);
 
                         var responseMonitor = RestHelper.ExecutePostRequest<ListMonitorRetornoVM>(AppDefaults.UrlEmissaoNfeApi, "monitor", JsonConvert.SerializeObject(monitorVM), null, header);
                         if (responseMonitor == null)
                             continue;
 
-                        foreach (var itemNF in responseMonitor.Retornos)
-                        {
-                            //Atualiza Status NF;
-                            var nfe = NFeEntradaBL.Everything.Where(x => x.SefazId == itemNF.NotaId).FirstOrDefault();
-                            if (nfe != null)
-                            {
-                                nfe.Mensagem = null;
-                                nfe.Recomendacao = null;
-                                nfe.XML = null;
-
-                                nfe.Status = itemNF.Status;
-                                nfe.Mensagem = itemNF.Mensagem;
-                                nfe.Recomendacao = itemNF.Recomendacao;
-                            }
-                        }
+                        AtualizarStausNF(responseMonitor);
                     }
                 }
                 catch
@@ -99,11 +73,7 @@ namespace Fly01.Compras.BL
                                                         group nf by nf.PlataformaId into g
                                                         select new { plataformaId = g.Key, notaInicial = g.Min(x => x.SefazChaveAcesso), notaFinal = g.Max(x => x.SefazChaveAcesso) });
 
-            var header = new Dictionary<string, string>()
-            {
-                { "AppUser", AppUser },
-                { "PlataformaUrl", string.IsNullOrEmpty(plataformaUrl) ? PlataformaUrl : plataformaUrl }
-            };
+            var header = GetHeader(PlataformaUrl); 
 
             foreach (var dadosPlataforma in notasFiscaisInutilizadasByPlataforma)
             {
@@ -114,38 +84,15 @@ namespace Fly01.Compras.BL
                     if (dadosCertificado == null)
                         continue;
 
-
                     if (TotalTributacaoBL.ConfiguracaoTSSOK(dadosPlataforma.plataformaId))
                     {
-                        var monitorVM = new MonitorVM()
-                        {
-                            Homologacao = dadosCertificado.Homologacao,
-                            Producao = dadosCertificado.Producao,
-                            EntidadeAmbiente = dadosCertificado.EntidadeAmbiente,
-                            NotaInicial = dadosPlataforma.notaInicial.ToString(),
-                            NotaFinal = dadosPlataforma.notaFinal.ToString(),
-                        };
+                        var monitorVM = GetMonitorVM(dadosCertificado, dadosPlataforma);
 
                         var responseMonitor = RestHelper.ExecutePostRequest<ListMonitorRetornoVM>(AppDefaults.UrlEmissaoNfeApi, "monitor", JsonConvert.SerializeObject(monitorVM), null, header);
                         if (responseMonitor == null)
                             continue;
 
-                        foreach (var itemNF in (from r in responseMonitor.Retornos
-                                                join nfi in NotaFiscalInutilizadaBL.Everything on r.NotaId equals nfi.SefazChaveAcesso
-                                                select r))
-                        {
-                            //Atualiza Status NF inutilizada;
-                            var nfInutilizada = NotaFiscalInutilizadaBL.Everything.Where(x => x.SefazChaveAcesso == itemNF.NotaId).FirstOrDefault();
-                            if (nfInutilizada != null)
-                            {
-                                nfInutilizada.Mensagem = null;
-                                nfInutilizada.Recomendacao = null;
-
-                                nfInutilizada.Status = (StatusNotaFiscal)System.Enum.Parse(typeof(StatusNotaFiscal), itemNF.Status.ToString());
-                                nfInutilizada.Mensagem = itemNF.Mensagem;
-                                nfInutilizada.Recomendacao = itemNF.Recomendacao;
-                            }
-                        }
+                        AtualizarStatusNFInutilizadas(responseMonitor);
                     }
                 }
                 catch
@@ -163,12 +110,7 @@ namespace Fly01.Compras.BL
                                     group nf by nf.PlataformaId into g
                                     select new { plataformaId = g.Key });
 
-            var header = new Dictionary<string, string>()
-            {
-                { "AppUser", AppUser },
-                { "PlataformaUrl", string.IsNullOrEmpty(plataformaUrl) ? PlataformaUrl : plataformaUrl }
-            };
-
+            var header = GetHeader(PlataformaUrl); 
 
             foreach (var dadosPlataforma in groupPlataformas)
             {
@@ -186,22 +128,15 @@ namespace Fly01.Compras.BL
 
                         foreach (var cartaCorrecao in cartasCorrecoesByPlataforma)
                         {
-                            var monitorEventoVM = new MonitorEventoVM()
-                            {
-                                Homologacao = dadosCertificado.Homologacao,
-                                Producao = dadosCertificado.Producao,
-                                EntidadeAmbiente = dadosCertificado.EntidadeAmbiente,
-                                IdEvento = cartaCorrecao.IdRetorno,
-                                SefazChaveAcesso = cartaCorrecao.NotaFiscal.SefazId
-                            };
+                            var monitorEventoVM = GetMonitorEventoVM(dadosCertificado, cartaCorrecao);
 
                             var responseMonitor = RestHelper.ExecutePostRequest<MonitorEventoRetornoVM>(AppDefaults.UrlEmissaoNfeApi, "monitorevento", JsonConvert.SerializeObject(monitorEventoVM), null, header);
                             if (responseMonitor == null)
                                 continue;                           
                             
                             cartaCorrecao.Mensagem = string.Format("{0} {1}",
-                                (responseMonitor.Motivo != null ? responseMonitor.Motivo : ""),
-                                (responseMonitor.MotivoEvento != null ? responseMonitor.MotivoEvento : ""));
+                                (responseMonitor.Motivo ?? ""),
+                                (responseMonitor.MotivoEvento ?? ""));
 
                             cartaCorrecao.Status = responseMonitor.Status;
                             cartaCorrecao.XML = responseMonitor.XML;
@@ -220,6 +155,78 @@ namespace Fly01.Compras.BL
                     continue;
                 }
             }
+        }
+
+        private Dictionary<string, string> GetHeader(string plataformaUrl)
+        {
+            return new Dictionary<string, string>()
+            {
+                { "AppUser", AppUser },
+                { "PlataformaUrl", string.IsNullOrEmpty(plataformaUrl) ? PlataformaUrl : plataformaUrl }
+            };
+        }
+
+        private void AtualizarStausNF(ListMonitorRetornoVM responseMonitor)
+        {
+            foreach (var itemNF in responseMonitor.Retornos)
+            {
+                //Atualiza Status NF;
+                var nfe = NFeEntradaBL.Everything.Where(x => x.SefazId == itemNF.NotaId).FirstOrDefault();
+                if (nfe != null)
+                {
+                    nfe.Mensagem = null;
+                    nfe.Recomendacao = null;
+                    nfe.XML = null;
+
+                    nfe.Status = itemNF.Status;
+                    nfe.Mensagem = itemNF.Mensagem;
+                    nfe.Recomendacao = itemNF.Recomendacao;
+                }
+            }
+        }
+
+        private void AtualizarStatusNFInutilizadas(ListMonitorRetornoVM responseMonitor)
+        {
+            foreach (var itemNF in (from r in responseMonitor.Retornos
+                                    join nfi in NotaFiscalInutilizadaBL.Everything on r.NotaId equals nfi.SefazChaveAcesso
+                                    select r))
+            {
+                //Atualiza Status NF inutilizada;
+                var nfInutilizada = NotaFiscalInutilizadaBL.Everything.Where(x => x.SefazChaveAcesso == itemNF.NotaId).FirstOrDefault();
+                if (nfInutilizada != null)
+                {
+                    nfInutilizada.Mensagem = null;
+                    nfInutilizada.Recomendacao = null;
+
+                    nfInutilizada.Status = (StatusNotaFiscal)System.Enum.Parse(typeof(StatusNotaFiscal), itemNF.Status.ToString());
+                    nfInutilizada.Mensagem = itemNF.Mensagem;
+                    nfInutilizada.Recomendacao = itemNF.Recomendacao;
+                }
+            }
+        }
+
+        private MonitorVM GetMonitorVM(EntidadeVM dadosCertificado, dynamic dadosPlataforma)
+        {
+            return new MonitorVM()
+            {
+                Homologacao = dadosCertificado.Homologacao,
+                Producao = dadosCertificado.Producao,
+                EntidadeAmbiente = dadosCertificado.EntidadeAmbiente,
+                NotaInicial = dadosPlataforma.notaInicial.ToString(),
+                NotaFinal = dadosPlataforma.notaFinal.ToString(),
+            };
+        }
+
+        private MonitorEventoVM GetMonitorEventoVM(EntidadeVM dadosCertificado, NotaFiscalCartaCorrecaoEntrada cartaCorrecao)
+        {
+            return new MonitorEventoVM()
+            {
+                Homologacao = dadosCertificado.Homologacao,
+                Producao = dadosCertificado.Producao,
+                EntidadeAmbiente = dadosCertificado.EntidadeAmbiente,
+                IdEvento = cartaCorrecao.IdRetorno,
+                SefazChaveAcesso = cartaCorrecao.NotaFiscal.SefazId
+            };
         }
     }
 }

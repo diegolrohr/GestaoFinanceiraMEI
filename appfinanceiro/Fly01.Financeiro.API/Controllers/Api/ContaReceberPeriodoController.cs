0d8406a3f69e978de24f9191d971b4b3138efa47
Vitor Kern <vitor.kern@totvs.com.br>
2018-11-16 17:28:34 -0200

    dev
    dev

---
+++ appfinanceiro/Fly01.Financeiro.API/Controllers/Api/ContaReceberPeriodoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using Fly01.Financeiro.BL;
+using Fly01.Core.API;
+
+namespace Fly01.Financeiro.API.Controllers.Api
+{
+    [RoutePrefix("contareceberperiodo")]
+    public class ContaReceberPeriodoController : ApiBaseController
+    {
+        [HttpGet]
+        public IHttpActionResult Get(DateTime dataInicial, DateTime dataFinal)
+        {
+            using (UnitOfWork unitOfWork = new UnitOfWork(ContextInitialize))
+            {
+                return
+                    Ok(
+                        new
+                        {
+                            value = unitOfWork.ContaReceberBL.Everything.Where(x =>
+                                (x.DataInclusao >= dataInicial && x.DataInclusao <= dataFinal) ||
+                                (x.DataAlteracao >= dataInicial && x.DataAlteracao <= dataFinal) ||
+                                (x.DataExclusao >= dataInicial && x.DataExclusao <= dataFinal))
+                        }
+                    );
+            }
+        }
+    }
+}

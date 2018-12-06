8240c413ba493e841b4e98adbbb749cc86809ea4
diego.rohr <diego.rohr@totvs.com.br>
2018-12-06 17:09:44 -0200

    alter tamanho campo complemento

---
+++ Fly01.Core.Entities/Domains/Commons/Treinamento.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fly01.Core.Entities.Domains.Commons
+{
+    public class Treinamento : PlataformaBase
+    {
+        [Required]
+        [StringLength(200, ErrorMessage = "O campo {0} deve possuir entre {2} e {1} caracteres.")]
+        public string Descricao { get; set; }
+
+        public int Numero { get; set; }
+    }
+}

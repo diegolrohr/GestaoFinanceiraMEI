829996e2a1a6ade9a8a451c906246a2a83ce913e
Honatel <marcos.honatel@totvs.com.br>
2018-12-11 13:47:07 -0200

    Ajustes

---
+++ Fly01.Core.Entities/Domains/Commons/AutenticacaoStone.cs
@@ -0,0 +1,14 @@
+﻿using Fly01.Core.Entities.Domains.Enum;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fly01.Core.Entities.Domains.Commons
+{
+    public class AutenticacaoStone
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password{ get; set; }
+    }
+}
--- appfinanceiro/Fly01.Financeiro.API/Controllers/Api/StoneController.cs
+++ appfinanceiro/Fly01.Financeiro.API/Controllers/Api/StoneController.cs
@@ -1,11 +1,46 @@
 ﻿using Fly01.Core.API;
+using Fly01.Core.Notifications;
+using Fly01.Core.Rest;
+using System;
 using System.Web.Http;
+using RestSharp;
+using Fly01.Core.Entities.Domains.Commons;
 
 namespace Fly01.Financeiro.API.Controllers.Api
 {
     [RoutePrefix("stone")]
     public class StoneController : ApiBaseController
     {
+        public IHttpActionResult Get()
+        {
+            return Ok("SUCESSO");
+        }
 
+        //[HttpPost]
+        //public IHttpActionResult UserAutentication(AutenticacaoStone autenticacao)
+        //{
+        //    var url = "https://portalapi.stone.com.br/authenticate";
+        //    try
+        //    {
+        //        var teste = "";
+        //        RestClient client = new RestClient(url);
+        //        RestRequest request = CreateJsonRequest(teste);
+        //        //client.para
+
+        //        return null;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw new BusinessException("Não foi possível obter autenticação na stone. " + ex.Message);
+        //    }
+        //}
+
+        private static RestRequest CreateJsonRequest(string resource)
+        {
+            RestRequest request = new RestRequest();
+            request.Resource = resource;
+            request.RequestFormat = DataFormat.Json;
+            return request;
+        }
     }
 }

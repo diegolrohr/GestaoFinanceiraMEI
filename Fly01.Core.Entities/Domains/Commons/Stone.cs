3dd01dfb961fe735b4ac246a380c4ac1753c3a40
Cássio Tomilin <cassio.tomilin@totvs.com.br>
2018-12-11 13:36:58 -0200

    Domain Stone
    Domain Stone

---
+++ Fly01.Core.Entities/Domains/Commons/Stone.cs
@@ -0,0 +1,12 @@
+﻿namespace Fly01.Core.Entities.Domains.Commons
+{
+    public class Stone : PlataformaBase
+    {
+        public string ValorRecebido { get; set; }
+
+        public string ValorAntecipado { get; set; }
+
+        public string Taxa { get; set; }
+
+    }
+}

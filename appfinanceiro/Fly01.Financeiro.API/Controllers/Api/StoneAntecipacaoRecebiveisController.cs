3223dfbc791122694114d1a71bd9c6af64cad365
Cássio Tomilin <cassio.tomilin@totvs.com.br>
2018-12-12 13:38:05 -0200

    Criando nova controller api + BL

---
+++ appfinanceiro/Fly01.Financeiro.API/Controllers/Api/StoneAntecipacaoRecebiveisController.cs
@@ -0,0 +1,12 @@
+﻿using Fly01.Core.Entities.Domains.Commons;
+using Fly01.Financeiro.BL;
+using System.Web.Http;
+
+namespace Fly01.Financeiro.API.Controllers.Api
+{
+
+    [RoutePrefix("stoneantecipacaorecebiveis")]
+    public class StoneAntecipacaoRecebiveisController : ApiPlataformaController<StoneAntecipacaoRecebiveis, StoneAntecipacaoRecebiveisBL>
+    {
+    }
+}

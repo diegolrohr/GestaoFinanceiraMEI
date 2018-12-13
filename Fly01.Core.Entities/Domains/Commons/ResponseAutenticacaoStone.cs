da2bc9ffb8cdeed63969e9e81f3943989d613f1c
diego.rohr <diego.rohr@totvs.com.br>
2018-12-13 14:45:45 -0200

    ValidarToken

--- Fly01.Core.Entities/Domains/Commons/ResponseAutenticacaoStone.cs
+++ Fly01.Core.Entities/Domains/Commons/ResponseAutenticacaoStone.cs
@@ -1,6 +1,12 @@
-﻿namespace Fly01.Core.Entities.Domains.Commons
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fly01.Core.Entities.Domains.Commons
 {
-    public class ResponseAutenticacaoStone : StoneTokenBase
+    public class ResponseAutenticacaoStone
     {
+        [Required]
+        [JsonProperty("token")]
+        public string Token { get; set; }
     }
 }

3592e53da8340458f18614a2ed983b2b29af468d
Alexis <alexisdiel@gmail.com>
2018-12-18 22:23:40 -0200

    Auto stash before merge of "stone_web_session" and "origin/stone_web"

---
+++ Fly01.Core/Config/StoneDataVM.cs
@@ -0,0 +1,18 @@
+﻿using Fly01.Core.ViewModels.Presentation.Commons;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Fly01.Core.Config
+{
+    public class StoneDataVM
+    {
+        public StoneDataVM()
+        {
+            Simulacoes = new List<ResponseAntecipacaoStoneVM>();
+        }
+        public string Token { get; set; }
+        
+        [JsonIgnore]
+        public List<ResponseAntecipacaoStoneVM> Simulacoes { get; set; }
+    }
+}

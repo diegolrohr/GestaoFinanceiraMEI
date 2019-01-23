234da5e5454af4a4075f2d84cb20024ec755e679
Cássio Tomilin <cassio.tomilin@totvs.com.br>
2019-01-23 09:51:04 -0200

    Calendar

--- appordemServico/Fly01.OrdemServico/Controllers/OrdemServicoController.cs
+++ appordemServico/Fly01.OrdemServico/Controllers/OrdemServicoController.cs
@@ -198,6 +198,7 @@ namespace Fly01.OrdemServico.Controllers
                 target.Add(new HtmlUIButton { Id = "cancel", Label = "Cancelar", OnClickFn = "fnCancelarNovaOrdem", Position = HtmlUIButtonPosition.Main });
                 target.Add(new HtmlUIButton { Id = "andamento", Label = "Executar", OnClickFn = "fnAlterarStatusAndamento", Position = HtmlUIButtonPosition.Out });
                 target.Add(new HtmlUIButton { Id = "concluido", Label = "Concluir", OnClickFn = "fnAlterarStatusConcluido", Position = HtmlUIButtonPosition.Out });
+                target.Add(new HtmlUIButton { Id = "save", Label = "Salvar", OnClickFn = "fnSalvar", Position = HtmlUIButtonPosition.Out });
             }
 
             return target;

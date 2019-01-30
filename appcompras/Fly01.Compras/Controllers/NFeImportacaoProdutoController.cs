deed847b2459d9aa26f695a2ecf096b2f8b7246a
DIEGO ROHR <DIEGO.ROHR@TOTVS.COM.BR>
2019-01-30 20:26:58 -0200

    modal fator conversao

--- appcompras/Fly01.Compras/Controllers/NFeImportacaoProdutoController.cs
+++ appcompras/Fly01.Compras/Controllers/NFeImportacaoProdutoController.cs
@@ -8,6 +8,8 @@ using Fly01.Core.Rest;
 using Fly01.Core.ViewModels;
 using Fly01.Core.ViewModels.Presentation.Commons;
 using Fly01.uiJS.Classes;
+using Fly01.uiJS.Classes.Elements;
+using Fly01.uiJS.Classes.Helpers;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -90,6 +92,135 @@ namespace Fly01.Compras.Controllers
             };
             //$expand = produto($expand = unidadeMedida),unidadeMedida &$filter = ((produtoId eq null) or(produto ne null and produto / unidadeMedida / abreviacao ne unidadeMedida / abreviacao))
             return GridLoad(filters);
+        }
+
+        [HttpGet]
+        public ContentResult ModalFatorConversao(string id, string unidadeXml, string unidadeCadastro, double valor, double quantidade, double fatorConversao, string tipoFatorConversao)
+        { 
+            ModalUIForm config = new ModalUIForm()
+            {
+                Title = "Fator de Conversão",
+                UrlFunctions = @Url.Action("Functions") + "?fns=",
+                ConfirmAction = new ModalUIAction() { Label = "Salvar" },
+                CancelAction = new ModalUIAction() { Label = "Cancelar" },
+                Action = new FormUIAction
+                {
+                },
+                Id = "fly01mdlfrmFatorConversao",
+                //ReadyFn = "fnFormReadyFatorConversao",
+                //Functions = new List<string>() { "fnChangeFatorConversao" }
+            };
+
+            config.Elements.Add(new InputHiddenUI { Id = "id", Value = id });
+            config.Elements.Add(new ButtonGroupUI()
+            {
+                Id = "fly01btngrpTipoFatorConversao",
+                Class = "col s12 m12",
+                //OnClickFn = "fnChangeTipoFatorConversao",
+                Label = "Tipo do fator de conversão",
+                Options = new List<ButtonGroupOptionUI>
+                {
+                    new ButtonGroupOptionUI { Id = "btnMultiplicar", Label = "Multiplicar (X)"},
+                    new ButtonGroupOptionUI { Id = "btnDividir", Label = "Dividir (/)"},
+                }
+            });
+            config.Elements.Add(new LabelSetUI { Id = "labelSetXml", Label = "Dados do xml" });
+            config.Elements.Add(new InputTextUI
+            {
+                Id = "unidadeXml",
+                Class = "col s12 l2",
+                Label = "Unidade Xml",
+                Value = unidadeXml,
+                Disabled = true
+            });
+
+            config.Elements.Add(new InputFloatUI
+            {
+                Id = "quantidade",
+                Class = "col s12 l4 numeric",
+                Label = "Quantidade",
+                Digits = 2,
+                Value = quantidade.ToString(),
+                Disabled = true
+            });
+
+            config.Elements.Add(new InputFloatUI
+            {
+                Id = "valor",
+                Class = "col s12 l3 numeric",
+                Label = "Valor Unitário",
+                Digits = 2,
+                Value = valor.ToString(),
+                Disabled = true
+            });
+            config.Elements.Add(new InputFloatUI
+            {
+                Id = "quantidadeConvertida",
+                Class = "col s12 l3 numeric",
+                Label = "Quantidade Final",
+                Digits = 2,
+                Value = valor.ToString(),
+                Disabled = true
+            });
+
+            config.Elements.Add(new LabelSetUI { Id = "labelSetXml", Label = "Dados para cadastro" });
+            config.Elements.Add(new InputTextUI
+            {
+                Id = "unidadeCadastro",
+                Class = "col s12 l2",
+                Label = "Unidade Cadastro",
+                Value = unidadeCadastro,
+                Disabled = true
+            });
+                        
+            config.Elements.Add(new InputFloatUI
+            {
+                Id = "fatorConversao",
+                Class = "col s12 l4 numeric",
+                Label = "Fator Conversão",
+                Digits = 2,
+                Value = valor.ToString(),
+                Required = true
+            });
+            config.Elements.Add(new InputFloatUI
+            {
+                Id = "valorConvertido",
+                Class = "col s12 l3 numeric",
+                Label = "Valor Unitário Final",
+                Digits = 2,
+                Value = valor.ToString(),
+                Disabled = true
+            });
+            config.Elements.Add(new InputFloatUI
+            {
+                Id = "quantidadeConvertida",
+                Class = "col s12 l3 numeric",
+                Label = "Quantidade Final",
+                Digits = 2,
+                Value = valor.ToString(),
+                Disabled = true
+            });
+
+            #region Helpers 
+            config.Helpers.Add(new TooltipUI
+            {
+                Id = "unidadeCadastro",
+                Tooltip = new HelperUITooltip()
+                {
+                    Text = "Unidade de medida cadastrada no produto vinculado."
+                }
+            });
+            config.Helpers.Add(new TooltipUI
+            {
+                Id = "fatorConversao",
+                Tooltip = new HelperUITooltip()
+                {
+                    Text = "Fator de conversão a ser utilizado para movimentar estoque e atualizar o valor de compra/venda no cadastro do produto."
+                }
+            });
+            #endregion
+
+            return Content(JsonConvert.SerializeObject(config, JsonSerializerSetting.Front), "application/json");
         }
 
         [OperationRole(PermissionValue = EPermissionValue.Write)]

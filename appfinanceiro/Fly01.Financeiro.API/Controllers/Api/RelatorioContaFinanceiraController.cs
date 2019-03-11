e461174d41ddb84812fd59507a856cd9a9381a29
cassio.tomilin <cassio.tomilin@totvs.com.br>
2019-03-11 14:15:22 +0000

    Merged PR 7963: Ajustes Relatorio CP/CR
    Ajustes Relatorio CP/CR
    
    Ajustes Relatorio CP/CR

--- appfinanceiro/Fly01.Financeiro.API/Controllers/Api/RelatorioContaFinanceiraController.cs
+++ appfinanceiro/Fly01.Financeiro.API/Controllers/Api/RelatorioContaFinanceiraController.cs
@@ -6,6 +6,7 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.Web.Http;
+using Fly01.Core.Notifications;
 
 namespace Fly01.Financeiro.API.Controllers.Api
 {
@@ -17,19 +18,19 @@ namespace Fly01.Financeiro.API.Controllers.Api
             return x => new ImprimirListContasVM()
             {
                 Id = x.Id,
-                Descricao = x.Descricao,
-                Categoria = x.Categoria.Descricao,
-                CentroCusto = x.CentroCusto != null ? x.CentroCusto.Descricao : "",
+                Descricao = x.Descricao.Length > 15 ? x.Descricao.Substring(1,15) : x.Descricao,
+                Categoria = x.Categoria.Descricao.Length > 15 ? x.Categoria.Descricao.Substring(1, 10) : x.Categoria.Descricao,
+                CentroCusto = x.CentroCusto == null ? "" : x.CentroCusto.Descricao.Length > 13 ? x.CentroCusto.Descricao.Substring(1,13) : x.CentroCusto.Descricao,
                 Status = x.StatusContaBancaria.ToString() == "EmAberto" ? "Em Aberto" : x.StatusContaBancaria.ToString() == "BaixadoParcialmente" ? "Baixado Parcialmente" : x.StatusContaBancaria.ToString(),
                 Valor = x.ValorPrevisto,
-                FormaPagamento = x.FormaPagamento != null ? x.FormaPagamento.Descricao : string.Empty,
-                Fornecedor = x.Pessoa != null ? x.Pessoa.Nome : string.Empty,
+                FormaPagamento = x.FormaPagamento == null ?  string.Empty : x.FormaPagamento.Descricao.Length > 13 ? x.FormaPagamento.Descricao.Substring(1,13) : x.FormaPagamento.Descricao,
+                Fornecedor = x.Pessoa == null ? string.Empty : x.Pessoa.Nome.Length > 12 ? x.Pessoa.Nome.Substring(1,12) : x.Pessoa.Nome,
                 Cliente = x.Pessoa != null ? x.Pessoa.Nome : string.Empty,
                 Vencimento = x.DataVencimento,
-                Titulo = tipoReport == "ContaPagar" ? "Conta a pagar" : "Conta a receber",
+                Titulo = tipoReport == "ContaPagar" ? "Contas a Pagar" : "Contas a Receber",
                 Numero = x.Numero,
-                CondicaoParcelamento = x.CondicaoParcelamento.Descricao,
-                Parcela = x.DescricaoParcela,
+                CondicaoParcelamento = x.CondicaoParcelamento.Descricao.Length > 15 ? x.CondicaoParcelamento.Descricao.Substring(1,15) : x.CondicaoParcelamento.Descricao,
+                Parcela = x.DescricaoParcela.Length > 7 ? x.DescricaoParcela.Substring(1,7) : x.DescricaoParcela,
                 TipoConta = tipoReport,
                 Emissao = x.DataEmissao
             };
@@ -64,36 +65,53 @@ namespace Fly01.Financeiro.API.Controllers.Api
 
             using (var unitOfWork = new UnitOfWork(ContextInitialize))
             {
-                if (tipoConta == "ContaPagar")
+                try
                 {
-                    List<ImprimirListContasVM> result = unitOfWork.ContaPagarBL
-                       .AllIncluding(
-                           x => x.FormaPagamento,
-                           x => x.Pessoa,
-                           x => x.CentroCusto,
-                           x => x.CondicaoParcelamento,
-                           x => x.Categoria
-                       ).Where(filterPredicate)
-                       .Take(2000)
-                       .Select(GetDisplayData(tipoConta)).ToList();
+                    if (tipoConta == "ContaPagar")
+                    {
+                        List<ImprimirListContasVM> result = unitOfWork.ContaPagarBL
+                           .AllIncluding(
+                               x => x.FormaPagamento,
+                               x => x.Pessoa,
+                               x => x.CentroCusto,
+                               x => x.CondicaoParcelamento,
+                               x => x.Categoria
+                           ).Where(filterPredicate)
+                           .Take(2000)
+                           .Select(GetDisplayData(tipoConta)).ToList();
 
-                    return Ok(new { count = result.Count, value = result });
+                        if (result.Count == 0)
+                        {
+                            throw new BusinessException("Não existem contas à serem impressas. Por gentileza revise seus filtros e imprima novamente.");
+                        }
+
+                        return Ok(new { count = result.Count, value = result });
+                    }
+                    else
+                    {
+                        List<ImprimirListContasVM> result = unitOfWork.ContaReceberBL
+                        .AllIncluding(
+                            x => x.FormaPagamento,
+                            x => x.Pessoa,
+                            x => x.CentroCusto,
+                            x => x.CondicaoParcelamento,
+                            x => x.Categoria
+                        )
+                        .Where(filterPredicate)
+                        .Take(2000)
+                        .Select(GetDisplayData(tipoConta)).ToList();
+
+                        if (result.Count == 0)
+                        {
+                            throw new BusinessException("Não existem contas à serem impressas. Por gentileza revise seus filtros e imprima novamente.");
+                        }
+
+                        return Ok(new { count = result.Count, value = result });
+                    }
                 }
-                else
+                catch (Exception ex)
                 {
-                    List<ImprimirListContasVM> result = unitOfWork.ContaReceberBL
-                    .AllIncluding(
-                        x => x.FormaPagamento,
-                        x => x.Pessoa,
-                        x => x.CentroCusto,
-                        x => x.CondicaoParcelamento,
-                        x => x.Categoria
-                    )
-                    .Where(filterPredicate)
-                    .Take(2000)
-                    .Select(GetDisplayData(tipoConta)).ToList();
-
-                    return Ok(new { count = result.Count, value = result });
+                    throw new BusinessException(ex.Message);
                 }
             }
         }

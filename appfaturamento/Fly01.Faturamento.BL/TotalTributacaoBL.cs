e87ab6c55c8270e2656cc9670db892ced39c2f36
DIEGO ROHR <DIEGO.ROHR@TOTVS.COM.BR>
2019-03-27 17:07:00 -0300

    empresaPagaFrete

--- appfaturamento/Fly01.Faturamento.BL/TotalTributacaoBL.cs
+++ appfaturamento/Fly01.Faturamento.BL/TotalTributacaoBL.cs
@@ -449,12 +449,12 @@ namespace Fly01.Faturamento.BL
                     { "PlataformaUrl", PlataformaUrl }
                 };
 
-            bool calculaFrete = (
+            bool empresaPagaFrete = (
                 ((tipoFrete == TipoFrete.CIF || tipoFrete == TipoFrete.Remetente) && tipoVenda == TipoCompraVenda.Normal) ||
                 ((tipoFrete == TipoFrete.FOB || tipoFrete == TipoFrete.Destinatario) && tipoVenda == TipoCompraVenda.Devolucao)
             );
 
-            double freteFracionado = calculaFrete && valorFrete.HasValue ? valorFrete.Value / tributacaoItens.Sum(x => x.Quantidade) : 0;
+            double freteFracionado = valorFrete.HasValue ? valorFrete.Value / tributacaoItens.Sum(x => x.Quantidade) : 0.0;
 
             var num = 1;
             foreach (var itemProduto in tributacaoItens)

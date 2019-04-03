e6800db2bf5146a083d90d8c31e11c529faaa97b
DIEGO ROHR <DIEGO.ROHR@TOTVS.COM.BR>
2019-04-03 11:21:41 -0300

    fn change tipo enquadramento

--- Fly01.Core.Presentation/Controllers/AliquotaSimplesNacionalBaseController.cs
+++ Fly01.Core.Presentation/Controllers/AliquotaSimplesNacionalBaseController.cs
@@ -13,6 +13,7 @@ using System.Web.Mvc;
 namespace Fly01.Core.Presentation.Controllers
 {
     //$$$.modal("/AliquotaSimplesNacional/FormModal?idParametro=00000000-0000-0000-0000-000000000000")
+    //$$$.modal("/AliquotaSimplesNacional/FormModal?isOnCadastroParametros=false")
     public class AliquotaSimplesNacionalBaseController<T> : BaseController<T> where T : AliquotaSimplesNacionalVM
     {
         public override Func<T, object> GetDisplayData()
@@ -30,14 +31,14 @@ namespace Fly01.Core.Presentation.Controllers
             throw new NotImplementedException();
         }
 
-        public ContentResult FormModal(Guid idParametro = default(Guid))
+        public ContentResult FormModal(bool isOnCadastroParametros = false)
         {
             ModalUIForm config = new ModalUIForm()
             {
                 Title = "Alíquotas Simples Nacional",
                 UrlFunctions = @Url.Action("Functions") + "?fns=",
                 CancelAction = new ModalUIAction() { Label = "Cancelar" },
-                ConfirmAction = new ModalUIAction() { Label = (idParametro != default(Guid) ? "Aplicar" : "Salvar") },
+                ConfirmAction = new ModalUIAction() { Label = isOnCadastroParametros ? "Aplicar" : "Salvar" },
                 Action = new FormUIAction
                 {
                 },
@@ -45,31 +46,32 @@ namespace Fly01.Core.Presentation.Controllers
                 Id = "fly01mdlfrmAliquotaSimplesNacional"
             };
 
-            config.Elements.Add(new InputHiddenUI { Id = "id" });
+            config.Elements.Add(new InputHiddenUI { Id = "isOnCadastroParametros", Value = isOnCadastroParametros.ToString() });
             config.Elements.Add(new SelectUI
             {
                 Id = "tipoFaixaReceitaBruta",
                 Class = "col s12",
                 Label = "Faixa Receita Bruta",
                 Required = true,
-                Options = new List<SelectOptionUI>(SystemValueHelper.GetUIElementBase(typeof(TipoFaixaReceitaBruta)).ToList())
-                //DomEvents = new List<DomEventUI>
-                //{
-                //    new DomEventUI() { DomEvent = "change", Function = "fnChangeTipoFaixaReceitaBruta" }
-                //}
+                Options = new List<SelectOptionUI>(SystemValueHelper.GetUIElementBase(typeof(TipoFaixaReceitaBruta)).ToList()),
+                DomEvents = new List<DomEventUI>
+                {
+                    new DomEventUI() { DomEvent = "change", Function = "fnChangeTipoFaixaReceitaBruta" }
+                }
             });
 
             config.Elements.Add(new AutoCompleteUI
             {
                 Id = "tipoEnquadramentoEmpresa",
                 Class = "col s12",
+                Required = true,
                 Label = "Enquadramento Empresa",
                 DataUrl = Url.Action("AliquotaSimplesNacional", "AutoComplete"),
                 LabelId = "tipoEnquadramentoEmpresaDescricao",
                 PreFilter = "tipoFaixaReceitaBruta",
                 DomEvents = new List<DomEventUI>
                 {
-                    new DomEventUI() { DomEvent = "autocompleteselect", Function = "fnSelectTipoEnquadramentoEmpresa" }
+                    new DomEventUI() { DomEvent = "autocompleteselect", Function = "fnChangeTipoEnquadramentoEmpresa" }
                 }
             });
             config.Elements.Add(new InputCustommaskUI
@@ -106,15 +108,15 @@ namespace Fly01.Core.Presentation.Controllers
             });
             config.Elements.Add(new InputCustommaskUI
             {
-                Id = "pisPasep",
+                Id = "ipi",
                 Class = "col s12 m4",
-                Label = "PIS/PASEP",
+                Label = "IPI",
                 Disabled = true,
                 Data = new { inputmask = "'mask': '9{1,3}[,9{1,2}] %', 'alias': 'decimal', 'autoUnmask': true, 'suffix': ' %', 'radixPoint': ',' " }
             });
             config.Elements.Add(new InputCustommaskUI
             {
-                Id = "aliquotaISS",
+                Id = "iss",
                 Class = "col s12 m4",
                 Label = "Imposto Sobre Serviço",
                 Disabled = true,

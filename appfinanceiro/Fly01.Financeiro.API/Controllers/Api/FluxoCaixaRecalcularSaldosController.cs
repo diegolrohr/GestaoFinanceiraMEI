6af88e7f5a8415cec574e2319b789f1ef9c935da
DIEGO ROHR <DIEGO.ROHR@TOTVS.COM.BR>
2019-04-03 17:50:06 -0300

    DeleteWithoutRecalc

--- appfinanceiro/Fly01.Financeiro.API/Controllers/Api/FluxoCaixaRecalcularSaldosController.cs
+++ appfinanceiro/Fly01.Financeiro.API/Controllers/Api/FluxoCaixaRecalcularSaldosController.cs
@@ -4,6 +4,10 @@ using System.Web.Http;
 using Fly01.Financeiro.BL;
 using Fly01.Core.API;
 using Fly01.Core.Entities.Domains.Enum;
+using Fly01.Core.Entities.Domains.Commons;
+using Fly01.Core.BL;
+using Fly01.Core.Notifications;
+using System.Threading.Tasks;
 
 namespace Fly01.Financeiro.API.Controllers.Api
 {
@@ -42,28 +46,79 @@ namespace Fly01.Financeiro.API.Controllers.Api
         /// Posterior a execução do passo 1 no banco, realizar a chamada deste controller via postamn, para realizar o recálculo
         /// </summary>
         [HttpPost]
-        public IHttpActionResult Post()
+        public async Task<IHttpActionResult> Post()
         {
-            using (UnitOfWork unitOfWork = new UnitOfWork(ContextInitialize))
+            //TODO: Diego Segurança, logs
+            try
             {
-                #region Recuperar todas as contas a Pagar em aberto
+                using (UnitOfWork unitOfWork = new UnitOfWork(ContextInitialize))
+                {
+                    int countContaPagar = 0, countBaixaPagar = 0, countContaReceber = 0, countBaixaReceber = 0;
 
-                #endregion
-
-                #region Recuperar todas as contas a Pagar em aberto
-
-                #endregion
-
-                #region Inativar as baixas existentes e criar as novas para recálculo
-
-                #endregion
-
-                return Ok(
-                    new
+                    #region Recuperar todas as contas a Pagar com alguma baixa
+                    foreach (var contaPagar in unitOfWork.ContaPagarBL.All.Where(x => x.StatusContaBancaria == StatusContaBancaria.Pago || x.StatusContaBancaria == StatusContaBancaria.BaixadoParcialmente).OrderBy(x => x.DataVencimento))
                     {
-                        totalRecords = 15
+                        countContaPagar++;
+                        contaPagar.ValorPrevisto = Math.Abs(contaPagar.ValorPrevisto);
+                        contaPagar.ValorPago = 0.0;
+                        unitOfWork.ContaPagarBL.Update(contaPagar);
+
+                        foreach (var baixaPagar in unitOfWork.ContaFinanceiraBaixaBL.All.Where(x => x.ContaFinanceiraId == contaPagar.Id).OrderBy(x => x.Data))
+                        {
+                            countBaixaPagar++;
+                            unitOfWork.ContaFinanceiraBaixaBL.DeleteWithoutRecalc(baixaPagar);
+                            //verificar se no insert o .all já vai considerar o valor pago 0
+                            unitOfWork.ContaFinanceiraBaixaBL.Insert(new ContaFinanceiraBaixa()
+                            {
+                                ContaBancariaId = baixaPagar.ContaBancariaId,
+                                ContaFinanceiraId = baixaPagar.ContaFinanceiraId,
+                                Data = baixaPagar.Data,
+                                Observacao = baixaPagar.Observacao,
+                                Valor = Math.Abs(baixaPagar.Valor)
+                            });
+                        }
                     }
-                );
+                    #endregion
+
+                    #region Recuperar todas as contas a Receber com alguma baixa
+                    foreach (var contaReceber in unitOfWork.ContaReceberBL.All.Where(x => x.StatusContaBancaria == StatusContaBancaria.Pago || x.StatusContaBancaria == StatusContaBancaria.BaixadoParcialmente).OrderBy(x => x.DataVencimento))
+                    {
+                        countContaReceber++;
+                        contaReceber.ValorPrevisto = Math.Abs(contaReceber.ValorPrevisto);
+                        contaReceber.ValorPago = 0.0;
+                        unitOfWork.ContaReceberBL.Update(contaReceber);
+
+                        foreach (var baixaPagar in unitOfWork.ContaFinanceiraBaixaBL.All.Where(x => x.ContaFinanceiraId == contaReceber.Id).OrderBy(x => x.Data))
+                        {
+                            countBaixaReceber++;
+                            unitOfWork.ContaFinanceiraBaixaBL.DeleteWithoutRecalc(baixaPagar);
+                            //verificar se no insert o .all já vai considerar o valor pago 0
+                            unitOfWork.ContaFinanceiraBaixaBL.Insert(new ContaFinanceiraBaixa()
+                            {
+                                ContaBancariaId = baixaPagar.ContaBancariaId,
+                                ContaFinanceiraId = baixaPagar.ContaFinanceiraId,
+                                Data = baixaPagar.Data,
+                                Observacao = baixaPagar.Observacao,
+                                Valor = Math.Abs(baixaPagar.Valor)
+                            });
+                        }
+                    }
+                    #endregion
+
+                    await unitOfWork.Save();
+
+                    return Ok(new
+                    {
+                        totalContasPagarProcessadas = countContaPagar,
+                        totalBaixasPagarProcessadas = countBaixaPagar,
+                        totalContasReceber = countContaReceber,
+                        totalBaixasReceberProcessadas = countBaixaReceber
+                    });
+                }
+            }
+            catch(Exception ex)
+            {
+                return InternalServerError(ex);
             }
         }
     }

d0577a06b0b6c6a203a01c21244b636dd031c928
DIEGO ROHR <DIEGO.ROHR@TOTVS.COM.BR>
2019-04-10 10:11:51 -0300

    novo tss

---
+++ appemissaoNFE/Fly01.EmissaoNFE.Domain/Entities/NFe/Exportacao.cs
@@ -0,0 +1,24 @@
+﻿using Fly01.EmissaoNFE.Domain.Enums;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Fly01.EmissaoNFE.Domain.Entities.NFe
+{
+    public class Exportacao
+    {
+        /// <summary>
+        /// informar a sigla da UF de Embarque ou de transposição de fronteira. A UF de embarque é a UF do local onde será embarcada para o exterior (porto/aeroporto), 
+        /// no caso de ser transporte terrestre deve ser o local de transposição de fronteira. Não aceita o valor "EX".
+        /// </summary>
+        [XmlElement(ElementName = "UFSaidaPais")]
+        public string UFSaidaPais { get; set; }
+
+        /// <summary>
+        /// informar o local de embarque, local onde será embarcada para o exterior (porto/aeroporto), no caso de ser transporte terrestre deve ser 
+        /// o local de transposição de fronteira.
+        /// </summary>
+        [XmlElement(ElementName = "xLocEmbarq")]
+        public string xLocEmbarq { get; set; }
+    }
+}

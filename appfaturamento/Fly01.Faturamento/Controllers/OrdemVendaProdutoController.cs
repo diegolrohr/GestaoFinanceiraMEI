7fcbeb5efa15e01d2f688e452a87dcab60338e20
DIEGO ROHR <DIEGO.ROHR@TOTVS.COM.BR>
2019-04-11 16:46:51 -0300

    quantidade 3

--- appfaturamento/Fly01.Faturamento/Controllers/OrdemVendaProdutoController.cs
+++ appfaturamento/Fly01.Faturamento/Controllers/OrdemVendaProdutoController.cs
@@ -96,7 +96,7 @@ namespace Fly01.Faturamento.Controllers
                 Id = "quantidade",
                 Class = "col s12 l6 numeric",
                 Label = "Quantidade",
-                Digits = 2,//total vai divergir no xml da nota fiscal, aceita só 2 casas
+                Digits = 3,
                 Value = "1",
                 Required = false
             });

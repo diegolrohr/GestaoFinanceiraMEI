e7a0558113ef2de9e5e4a185942bdc08efeb0e7d
DIEGO ROHR <DIEGO.ROHR@TOTVS.COM.BR>
2019-04-12 14:25:21 -0300

    remoção validação grupo tribuário

--- appcompras/Fly01.Compras.BL/PedidoItemBL.cs
+++ appcompras/Fly01.Compras.BL/PedidoItemBL.cs
@@ -22,9 +22,6 @@ namespace Fly01.Compras.BL
             entity.Fail(entity.Total < 0, new Error("O Total não pode ser negativo", "total"));
             entity.Fail(Math.Round(entity.ValorCreditoICMS, 2) > Math.Round((entity.Quantidade * entity.Valor), 2), new Error("O valor do crédito ICMS não pode ser superior ao total bruto", "valorCreditoICMS"));
 
-            var jaExiste = All.Any(x => x.PedidoId == entity.PedidoId && x.ProdutoId == entity.ProdutoId && x.GrupoTributarioId == entity.GrupoTributarioId && x.Id != entity.Id);
-            entity.Fail(jaExiste, new Error("Este produto já está adicionado a este pedido"));
-
             base.ValidaModel(entity);
         }
     }

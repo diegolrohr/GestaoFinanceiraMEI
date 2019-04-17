dd5ca724534810c2aa02eab929c63b66b0a4d2cf
DIEGO ROHR <DIEGO.ROHR@TOTVS.COM.BR>
2019-04-17 09:41:28 -0300

    validacao produto notas

--- appcompras/Fly01.Compras.BL/NFeProdutoEntradaBL.cs
+++ appcompras/Fly01.Compras.BL/NFeProdutoEntradaBL.cs
@@ -20,9 +20,6 @@ namespace Fly01.Compras.BL
             entity.Fail(entity.Desconto >= (entity.Quantidade * entity.Valor), new Error("O Desconto não pode ser maior ou igual ao total", "desconto"));
             entity.Fail(entity.Total <= 0, new Error("O Total deve ser superior a zero", "total"));
 
-            var jaExiste = All.Any(x => x.NotaFiscalEntradaId == entity.NotaFiscalEntradaId && x.ProdutoId == entity.ProdutoId && x.GrupoTributarioId == entity.GrupoTributarioId && x.Id != entity.Id);
-            entity.Fail(jaExiste, new Error("Este produto com este grupo tributário já está adicionado a esta nota fiscal"));
-
             base.ValidaModel(entity);
         }
     }

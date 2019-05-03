5e7ec996d3811625d144026bffac97395131c531
DIEGO ROHR <DIEGO.ROHR@TOTVS.COM.BR>
2019-05-03 14:42:04 -0300

    CodigoIBGEPrestador

--- appemissaoNFE/Fly01.EmissaoNFE.Domain/Entities/NFs/Atividade.cs
+++ appemissaoNFE/Fly01.EmissaoNFE.Domain/Entities/NFs/Atividade.cs
@@ -8,8 +8,19 @@ namespace Fly01.EmissaoNFE.Domain.Entities.NFS
         [XmlElement(ElementName = "codigo")]
         public string CodigoCNAE { get; set; }
 
+        /// <summary>
+        /// 3547809	Santo André SP não deve sair o CNAE
+        /// </summary>
+        private bool ShouldSerializeCodigoCNAE()
+        {
+            return (CodigoIBGEPrestador != "3547809");
+        }
+
         [XmlIgnore]
         public double AliquotaIss { get; set; }
+
+        [XmlIgnore]
+        public string CodigoIBGEPrestador { get; set; }
 
         [XmlElement(ElementName = "aliquota")]
         public string AliquotaIssString

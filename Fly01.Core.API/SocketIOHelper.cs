cfef8fce65a5238dd422ab55c9f5f2fa22752490
DIEGO ROHR <DIEGO.ROHR@TOTVS.COM.BR>
2019-05-21 11:46:23 -0300

    SocketIOHelper

---
+++ Fly01.Core.API/SocketIOHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Fly01.Core.Notifications;
+using Quobject.SocketIoClientDotNet.Client;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Fly01.Core.ViewModels.Presentation.Commons;
+
+namespace Fly01.Core.API
+{
+    public class SocketIOHelper
+    {
+        public static void Emit(string emitEvent, SocketMessageVM message)
+        {
+            try
+            {
+                var socket = IO.Socket("AppDefaults");
+                socket.Connect();
+
+                socket.Emit(emitEvent, message);
+
+                socket.Disconnect();
+            }
+            catch (Exception ex)
+            {
+                throw new BusinessException(ex?.Message);
+            }
+        }
+    }
+}
---
+++ Fly01.Core.Entities/Domains/Enum/SocketMessageType.cs
@@ -0,0 +1,9 @@
+﻿namespace Fly01.Core.Entities.Domains.Enum
+{
+    public enum SocketMessageType
+    {
+        WARNING = 0,
+        INFO = 1,
+        ERROR = 2
+    }
+}

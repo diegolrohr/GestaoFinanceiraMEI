09041bd234bde960caac450bd471d69c76ec4a58
DIEGO ROHR <DIEGO.ROHR@TOTVS.COM.BR>
2019-06-03 16:00:19 -0300

    responsavel 53113791000122

--- appemissaoNFE/Fly01.EmissaoNFE.DAL/Migrations/DataInitializer/ResponsavelTecnicoDataInitializer.cs
+++ appemissaoNFE/Fly01.EmissaoNFE.DAL/Migrations/DataInitializer/ResponsavelTecnicoDataInitializer.cs
@@ -16,20 +16,21 @@ namespace Fly01.EmissaoNFE.DAL.Migrations.DataInitializer
                 var lista = new List<ResponsavelTecnico>() {
                     new ResponsavelTecnico()
                     {
-                    Id = Guid.Parse("A39B871C-6913-495C-88F8-1F2668B6AABA"),
-                    UsuarioInclusao = "Seed",
-                    DataInclusao = DateTime.Now,
-                    CNPJ = "19116002000179",
-                    Contato = "Ramon Martins Da Silva",
-                    Email = "resp_tecnico_dfe_mpn@totvs.com.br",
-                     Fone = "1128593905",
+                        Id = Guid.Parse("A39B871C-6913-495C-88F8-1F2668B6AABA"),
+                        UsuarioInclusao = "Seed",
+                        DataInclusao = DateTime.Now,
+                        CNPJ = "19116002000179",
+                        Contato = "Ramon Martins Da Silva",
+                        Email = "resp_tecnico_dfe_mpn@totvs.com.br",
+                        Fone = "1128593905"
                     },
                     new ResponsavelTecnico()//PR
                     {
                         Id = Guid.Parse("D21F8B07-580B-47C9-BD1E-5DF033748D7C"),
                         UsuarioInclusao = "Seed",
                         DataInclusao = DateTime.Now,
-                        CNPJ = "06377049838",
+                        //CNPJ = "06377049838",
+                        CNPJ = "53113791000122",
                         Contato = "Wilson De Godoy Soares Junior",
                         Email = "resp_tecnico_dfe_mpn@totvs.com.br",
                         Fone = "1128593905",

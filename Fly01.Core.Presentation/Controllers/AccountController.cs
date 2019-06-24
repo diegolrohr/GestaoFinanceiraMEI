5c682a0a1d678095090d9ccf1edc1e69ef15490a
Alexis <alexis.diel@gmail.com>
2019-06-24 10:21:54 -0300

    Manager Url

--- Fly01.Core.Presentation/Controllers/AccountController.cs
+++ Fly01.Core.Presentation/Controllers/AccountController.cs
@@ -5,26 +5,21 @@ using Fly01.Core.Rest;
 using Fly01.Core.Config;
 using Fly01.Core.Helpers;
 using System.Web.Security;
-using System.Web.Configuration;
 using System.Collections.Generic;
-using Fly01.Core.Presentation.SSO;
 using Newtonsoft.Json;
+using Fly01.Core.Defaults;
+using Fly01.uiJS.Responses;
 
 namespace Fly01.Core.Presentation.Controllers
 {
     [AllowAnonymous]
     public abstract class AccountController : Controller
     {
-        //public ActionResult Login(string returnUrl, string email = "")
-        //{
-        //    return View("LoginSSO", new SSOGatewayRequest()
-        //    {
-        //        AssertionUrl = Url.Action("Assertion", "Account", null, Request.Url.Scheme),
-        //        AppId = WebConfigurationManager.AppSettings["GatewayUserName"],
-        //        AppPassword = WebConfigurationManager.AppSettings["GatewayPassword"],
-        //        SSOUrl = AppDefaults.UrlLoginSSO
-        //    });
-        //}
+        public ContentResult Platforms()
+        {
+            ResponseDataVM<LoginResponse> result = RestUtils.ExecuteGetRequest<ResponseDataVM<LoginResponse>>(AppDefaults.UrlManager, "account/platforms", new Dictionary<string, string>(){}, null);
+            return Content(JsonConvert.SerializeObject(result, JsonSerializerSetting.Default), "application/json");
+        }
 
         public ActionResult Login(string returnUrl, string email = "")
         {
@@ -34,56 +29,10 @@ namespace Fly01.Core.Presentation.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public ActionResult Assertion()
-        //{
-        //    var assertion = new AssertionResponseVM(Request.Form);
-
-        //    var dataPermission = RestUtils.ExecuteGetRequest<ResponseDataVM<DataUserPermissionVM>>(AppDefaults.UrlManager, "user/permissions", RestUtils.GetAuthHeader(assertion.TokenType + " " + assertion.AccessToken), new Dictionary<string, string>()
-        //    {
-        //        { "platformUrl", assertion.PlatformUrl },
-        //        { "email", assertion.UserEmail },
-        //    });
-
-        //    var tokenDataVM = new TokenDataVM
-        //    {
-        //        AccessToken = assertion.AccessToken,
-        //        ExpiresIn = assertion.ExpiresIn,
-        //        TokenType = assertion.TokenType,
-        //        Username = assertion.Username
-        //    };
-
-        //    UserDataVM userDataVM = new UserDataVM
-        //    {
-        //        PlatformUser = assertion.UserEmail,
-        //        PlatformUrl = assertion.PlatformUrl,
-        //        TokenData = tokenDataVM,
-        //        Permissions = dataPermission.Data.Items,
-        //        ClientToken = assertion.ClientToken
-        //    };
-
-        //    SessionManager.Current.UserData = userDataVM;
-
-        //    HttpCookie mpnData = new HttpCookie("mpndata") { Expires = DateTime.UtcNow.AddDays(2), Path = "/" };
-        //    mpnData.Values["UserName"] = SessionManager.Current.UserData.TokenData.Username;
-        //    mpnData.Values["UserEmail"] = assertion.UserEmail;
-        //    mpnData.Values["TrialUntil"] = assertion.Trial ? assertion.LicenseExpiration : "";
-
-        //    Response.Cookies.Add(mpnData);
-
-        //    FormsAuthentication.SetAuthCookie(SessionManager.Current.UserData.PlatformUser, false);
-
-        //    return RedirectToAction("Index", "Home");
-        //}
-
         public ActionResult LogOff()
         {
-            //var clientToken = SessionManager.Current.UserData.ClientToken;
-
             if (HttpContext.Session != null)
                 SystemLogOff(System.Web.HttpContext.Current);
-
-            //return Redirect($"{AppDefaults.UrlLogoutSSO}/{clientToken}");
             return Redirect(AppDefaults.UrlLogoutSSO);
         }
 

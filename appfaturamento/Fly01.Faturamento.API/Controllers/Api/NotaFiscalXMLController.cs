e55493b73bdd4ad85387f896939813007b12f2dc
vitor.kern <vitor.kern@totvs.com.br>
2019-06-25 17:41:49 +0000

    Merged PR 11422: Download XMLs

--- appfaturamento/Fly01.Faturamento.API/Controllers/Api/NotaFiscalXMLController.cs
+++ appfaturamento/Fly01.Faturamento.API/Controllers/Api/NotaFiscalXMLController.cs
@@ -27,5 +27,23 @@ namespace Fly01.Faturamento.API.Controllers.Api
                 throw new BusinessException(ex.Message);
             }
         }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> Get(DateTime dataInicial, DateTime dataFinal)
+        {
+            try
+            {
+                using (UnitOfWork unitOfWork = new UnitOfWork(ContextInitialize))
+                {
+                    var xml = unitOfWork.NotaFiscalBL.NotasFiscaisPeriodo(dataInicial, dataFinal);
+                    await unitOfWork.Save();
+                    return Ok(xml);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new BusinessException(ex.Message);
+            }
+        }
     }
 }

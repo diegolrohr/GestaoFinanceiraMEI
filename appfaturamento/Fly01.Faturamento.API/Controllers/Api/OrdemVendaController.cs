2e38543a2f94a23313f0ea8ef8dbcef043018b07
Vitor Kern <vitor.kern@totvs.com.br>
2019-07-08 16:00:26 -0300

    Dev

--- appfaturamento/Fly01.Faturamento.API/Controllers/Api/OrdemVendaController.cs
+++ appfaturamento/Fly01.Faturamento.API/Controllers/Api/OrdemVendaController.cs
@@ -7,6 +7,8 @@ using System;
 using System.Web.OData;
 using System.Data.Entity.Infrastructure;
 using System.Net;
+using System.Data.Entity;
+using System.Linq;
 
 namespace Fly01.Faturamento.API.Controllers.Api
 {
@@ -86,6 +88,44 @@ namespace Fly01.Faturamento.API.Controllers.Api
             await UnitSave();
 
             return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        private async Task AtualizaTotalAoIncluirOuAlterar(OrdemVendaItem entity = null)
+        {
+            using (var unitOfWork = new UnitOfWork(ContextInitialize))
+            {
+                var configuracaoPersonalizacao = unitOfWork.ConfiguracaoPersonalizacaoBL.All.AsNoTracking().FirstOrDefault();
+                var exibirTransportadora = configuracaoPersonalizacao != null ? configuracaoPersonalizacao.ExibirStepTransportadoraCompras : true;
+
+                var pedido = unitOfWork.OrdemVendaBL.Find(entity.OrdemVendaId);
+                var total = unitOfWork
+                                .OrdemVendaProdutoBL
+                                .All
+                                .Where(x => x.Id == entity.OrdemVendaId && x.Id != entity.Id)
+                                .ToList()
+                                .Select(x => new
+                                {
+                                    Total = Convert.ToDouble(Math.Round(x.Quantidade * x.Valor - x.Desconto, 2,
+                                        MidpointRounding.AwayFromZero))
+                                })
+                                .Sum(x => x.Total) + entity.Total;
+
+                total = unitOfWork
+                                .OrdemVendaServicoBL
+                                .All
+                                .Where(x => x.Id == entity.OrdemVendaId && x.Id != entity.Id)
+                                .ToList()
+                                .Select(x => new
+                                {
+                                    Total = Convert.ToDouble(Math.Round(x.Quantidade * x.Valor - x.Desconto, 2,
+                                        MidpointRounding.AwayFromZero))
+                                })
+                                .Sum(x => x.Total) + entity.Total;
+                //Criar migration criando o campo Total no OrdemVenda
+                //pedido.Total += total + (((pedido.TipoFrete == TipoFrete.FOB || pedido.TipoFrete == TipoFrete.Destinatario) && exibirTransportadora) ? (pedido.ValorFrete ?? 0.0) : 0.0);
+                unitOfWork.OrdemVendaBL.Update(pedido);
+                await unitOfWork.Save();
+            }
         }
     }
 }

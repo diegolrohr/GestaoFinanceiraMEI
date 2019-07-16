af0de423346a8b04ee10133ead4f8747f42944c3
Vitor Kern <vitor.kern@totvs.com.br>
2019-07-16 11:48:05 -0300

    Dev

--- appcompras/Fly01.Compras.DAL/Migrations/201907152038331_AlterTotalPedido.cs
+++ appcompras/Fly01.Compras.DAL/Migrations/201907152038331_AlterTotalPedido.cs
@@ -7,6 +7,9 @@ namespace Fly01.Compras.DAL.Migrations
     {
         public override void Up()
         {
+            Sql(@"Update dbo.OrdemCompra
+                Set Total = 0
+                Where Total is null");
             AlterColumn("dbo.OrdemCompra", "Total", c => c.Double(nullable: false, defaultValue: 0));
         }
         

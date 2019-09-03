dc1b84ddf053238fc8a7dc85a21f7d03190b7a32
diego.rohr <diegol.rohr@gmail.com>
2019-09-03 18:33:58 -0300

    deleted old migrations

--- appfinanceiro/Fly01.Financeiro.DAL/AppDataContext.cs
+++ appfinanceiro/Fly01.Financeiro.DAL/AppDataContext.cs
@@ -37,53 +37,53 @@ namespace Fly01.Financeiro.API.Models.DAL
             builder.Properties<string>()
                 .Configure(x => x.HasColumnType("varchar"));
 
-            builder.Entity<ContaFinanceira>()
-                .Map(m => m.ToTable("ContaFinanceira"))
-                .Map<ContaPagar>(m => m.ToTable("ContaPagar"))
-                .Map<ContaReceber>(m => m.ToTable("ContaReceber"));
+            //builder.Entity<ContaFinanceira>()
+            //    .Map(m => m.ToTable("ContaFinanceira"))
+            //    .Map<ContaPagar>(m => m.ToTable("ContaPagar"))
+            //    .Map<ContaReceber>(m => m.ToTable("ContaReceber"));
 
             //What does principal end of an association means in relationship
             //por causa da repeticaoPaiId, explicito via FluentAPI
-            builder.Entity<ContaFinanceira>()
-                .HasOptional(b => b.ContaFinanceiraParcelaPai)
-                .WithMany()
-                .HasForeignKey(b => b.ContaFinanceiraParcelaPaiId)
-                .WillCascadeOnDelete(false);
+            //builder.Entity<ContaFinanceira>()
+            //    .HasOptional(b => b.ContaFinanceiraParcelaPai)
+            //    .WithMany()
+            //    .HasForeignKey(b => b.ContaFinanceiraParcelaPaiId)
+            //    .WillCascadeOnDelete(false);
 
-            builder.Entity<RenegociacaoContaFinanceira>()
-                .Map(m => m.ToTable("RenegociacaoContaFinanceira"))
-                .Map<RenegociacaoContaFinanceiraOrigem>(m => m.ToTable("RenegociacaoContaFinanceiraOrigem"))
-                .Map<RenegociacaoContaFinanceiraRenegociada>(m => m.ToTable("RenegociacaoContaFinanceiraRenegociada"));
+            //builder.Entity<RenegociacaoContaFinanceira>()
+            //    .Map(m => m.ToTable("RenegociacaoContaFinanceira"))
+            //    .Map<RenegociacaoContaFinanceiraOrigem>(m => m.ToTable("RenegociacaoContaFinanceiraOrigem"))
+            //    .Map<RenegociacaoContaFinanceiraRenegociada>(m => m.ToTable("RenegociacaoContaFinanceiraRenegociada"));
 
-            builder.Entity<ContaFinanceiraRenegociacao>().Ignore(m => m.ContasFinanceirasOrigemIds);
-            builder.Entity<ContaFinanceira>().Ignore(m => m.Saldo);
-            builder.Entity<ContaFinanceira>().Ignore(m => m.ContaBancariaId);
-            builder.Entity<ContaFinanceira>().Ignore(m => m.ContaBancaria);
-            builder.Entity<ConciliacaoBancaria>().Ignore(m => m.Arquivo);
-            builder.Entity<ContaBancaria>().Ignore(m => m.CodigoBanco);
-            builder.Entity<SaldoHistorico>().MapToStoredProcedures();
+            //builder.Entity<ContaFinanceiraRenegociacao>().Ignore(m => m.ContasFinanceirasOrigemIds);
+            //builder.Entity<ContaFinanceira>().Ignore(m => m.Saldo);
+            //builder.Entity<ContaFinanceira>().Ignore(m => m.ContaBancariaId);
+            //builder.Entity<ContaFinanceira>().Ignore(m => m.ContaBancaria);
+            //builder.Entity<ConciliacaoBancaria>().Ignore(m => m.Arquivo);
+            //builder.Entity<ContaBancaria>().Ignore(m => m.CodigoBanco);
+            //builder.Entity<SaldoHistorico>().MapToStoredProcedures();
             //builder.Entity<ContaFinanceira>().MapToStoredProcedures();
         }
 
-        public DbSet<Pessoa> Pessoas { get; set; }
+        //public DbSet<Pessoa> Pessoas { get; set; }
         public DbSet<Banco> Bancos { get; set; }
-        public DbSet<ContaBancaria> ContasBancarias { get; set; }
-        public DbSet<Categoria> Categorias { get; set; }
-        public DbSet<CondicaoParcelamento> CondicoesParcelamento { get; set; }
-        public DbSet<ContaFinanceira> ContasFinanceiras { get; set; }
-        public DbSet<ContaPagar> ContasPagar { get; set; }
-        public DbSet<ContaReceber> ContasReceber { get; set; }
-        public DbSet<ContaFinanceiraBaixa> ContasFinanceirasBaixas { get; set; }
+        //public DbSet<ContaBancaria> ContasBancarias { get; set; }
+        //public DbSet<Categoria> Categorias { get; set; }
+        //public DbSet<CondicaoParcelamento> CondicoesParcelamento { get; set; }
+        //public DbSet<ContaFinanceira> ContasFinanceiras { get; set; }
+        //public DbSet<ContaPagar> ContasPagar { get; set; }
+        //public DbSet<ContaReceber> ContasReceber { get; set; }
+        //public DbSet<ContaFinanceiraBaixa> ContasFinanceirasBaixas { get; set; }
         public DbSet<Estado> Estados { get; set; }
         public DbSet<Cidade> Cidades { get; set; }
-        public DbSet<SaldoHistorico> SaldosHistorico { get; set; }
-        public DbSet<ConciliacaoBancaria> ConciliacoesBancarias { get; set; }
-        public DbSet<ConciliacaoBancariaItem> ConciliacaoBancariaItens { get; set; }
-        public DbSet<ConciliacaoBancariaItemContaFinanceira> ConciliacaoBancariaItemContasFinanceiras { get; set; }
-        public DbSet<FormaPagamento> FormasPagamento { get; set; }
-        public DbSet<MovimentacaoFinanceira> Movimentacao { get; set; }
-        public DbSet<ContaFinanceiraRenegociacao> ContasFinanceirasRenegociacoes { get; set; }
-        public DbSet<RenegociacaoContaFinanceiraOrigem> RenegociacaoContasFinanceirasOrigem { get; set; }
-        public DbSet<RenegociacaoContaFinanceiraRenegociada> RenegociacaoContasFinanceirasRenegociadas { get; set; }
+        //public DbSet<SaldoHistorico> SaldosHistorico { get; set; }
+        //public DbSet<ConciliacaoBancaria> ConciliacoesBancarias { get; set; }
+        //public DbSet<ConciliacaoBancariaItem> ConciliacaoBancariaItens { get; set; }
+        //public DbSet<ConciliacaoBancariaItemContaFinanceira> ConciliacaoBancariaItemContasFinanceiras { get; set; }
+        //public DbSet<FormaPagamento> FormasPagamento { get; set; }
+        //public DbSet<MovimentacaoFinanceira> Movimentacao { get; set; }
+        //public DbSet<ContaFinanceiraRenegociacao> ContasFinanceirasRenegociacoes { get; set; }
+        //public DbSet<RenegociacaoContaFinanceiraOrigem> RenegociacaoContasFinanceirasOrigem { get; set; }
+        //public DbSet<RenegociacaoContaFinanceiraRenegociada> RenegociacaoContasFinanceirasRenegociadas { get; set; }
     }
 }
--- appfinanceiro/Fly01.Financeiro.DAL/Migrations/Configuration.cs
+++ appfinanceiro/Fly01.Financeiro.DAL/Migrations/Configuration.cs
@@ -17,22 +17,22 @@ namespace Fly01.Financeiro.DAL.Migrations
         {
             try
             {
-             //   Debugger Seed;
-              //  if (System.Diagnostics.Debugger.IsAttached == false)
-              //      System.Diagnostics.Debugger.Launch();
+                //   Debugger Seed;
+                //  if (System.Diagnostics.Debugger.IsAttached == false)
+                //      System.Diagnostics.Debugger.Launch();
 
                 //new BancoDataInitializer().Initialize(context);
 
-              //  var estados = new EstadoDataInitializer();
-              //  estados.Initialize(context);
-              //  var ufs = estados.GetLstOfStates();
-              //  new CidadeDataInitializer1().Initialize(context, ufs);
-              //  new CidadeDataInitializer2().Initialize(context, ufs);
-              //  new CidadeDataInitializer3().Initialize(context, ufs);
-              //  new CidadeDataInitializer4().Initialize(context, ufs);
-              //  new CidadeDataInitializer5().Initialize(context, ufs);
-              //  new CidadeDataInitializer6().Initialize(context, ufs);
-              //  new CidadeDataInitializer7().Initialize(context, ufs);
+                var estados = new EstadoDataInitializer();
+                estados.Initialize(context);
+                //  var ufs = estados.GetLstOfStates();
+                //  new CidadeDataInitializer1().Initialize(context, ufs);
+                //  new CidadeDataInitializer2().Initialize(context, ufs);
+                //  new CidadeDataInitializer3().Initialize(context, ufs);
+                //  new CidadeDataInitializer4().Initialize(context, ufs);
+                //  new CidadeDataInitializer5().Initialize(context, ufs);
+                //  new CidadeDataInitializer6().Initialize(context, ufs);
+                //  new CidadeDataInitializer7().Initialize(context, ufs);
 
             }
             catch (DbEntityValidationException ex)

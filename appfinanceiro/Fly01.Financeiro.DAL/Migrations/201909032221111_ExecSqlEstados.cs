2677ab566a7939e1f6f28db69ff8342ede81bea3
diego.rohr <diegol.rohr@gmail.com>
2019-09-03 19:23:42 -0300

    InsertEstados.sql

--- appfinanceiro/Fly01.Financeiro.DAL/Migrations/201909032221111_ExecSqlEstados.cs
+++ appfinanceiro/Fly01.Financeiro.DAL/Migrations/201909032221111_ExecSqlEstados.cs
@@ -3,18 +3,18 @@ namespace Fly01.Financeiro.DAL.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class ExecSqlBancos : DbMigration
+    public partial class ExecSqlEstados : DbMigration
     {
         public override void Up()
         {
             string diretorio = (AppDomain.CurrentDomain.BaseDirectory).Replace("bin", "").Replace("Debug", "");
 
-            SqlFile(diretorio + @"Migrations\SQLScripts\InsertBancos.sql");
+            SqlFile(diretorio + @"Migrations\SQLScripts\InsertEstados.sql");
         }
-
+        
         public override void Down()
         {
-            Sql("delete from dbo.banco");
+            Sql("delete from dbo.estado");
         }
     }
 }
